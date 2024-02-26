82b886c3266fe702831095732387de03150b0d10
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 15:28:43 +0300

    add ModelMapperConfig and ApplicationRunner

---
+++ test-application/src/main/java/by/aurorasoft/testapp/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package by.aurorasoft.testapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ApplicationRunner {
+
+    public static void main(final String... args) {
+        run(ApplicationRunner.class, args);
+    }
+}
