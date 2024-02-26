1db38b74650fad9278e7ccd138279dbfcb2b4847
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 15:30:38 +0300

    add ObjectMapperConfig

---
+++ test-application/src/main/java/by/aurorasoft/testapp/config/ObjectMapperConfig.java
@@ -0,0 +1,19 @@
+package by.aurorasoft.testapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+}
