b0dc57aa4ecdc4b753c8f3097eb6a6f74e4f52a2
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 11:29:51 +0300

    refactor KafkaReplicationConsumer

--- src/main/java/by/aurorasoft/replicator/consumer/KafkaReplicationConsumer.java
+++ src/main/java/by/aurorasoft/replicator/consumer/KafkaReplicationConsumer.java
@@ -2,7 +2,6 @@ package by.aurorasoft.replicator.consumer;
 
 import by.aurorasoft.kafka.consumer.KafkaConsumerGenericRecordBatch;
 import by.aurorasoft.replicator.model.ReplicationType;
-import by.aurorasoft.replicator.model.TransportableReplication;
 import by.aurorasoft.replicator.model.replication.Replication;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
@@ -19,7 +18,7 @@ import static by.aurorasoft.replicator.model.TransportableReplication.Fields.dto
 import static by.aurorasoft.replicator.model.TransportableReplication.Fields.type;
 
 @RequiredArgsConstructor
-public abstract class KafkaConsumerReplication<ID, DTO extends AbstractDto<ID>>
+public abstract class KafkaReplicationConsumer<ID, DTO extends AbstractDto<ID>>
         extends KafkaConsumerGenericRecordBatch<ID, Replication<ID, DTO>> {
     private final AbsServiceCRUD<ID, ?, DTO, ?> service;
     private final ObjectMapper objectMapper;
@@ -29,26 +28,29 @@ public abstract class KafkaConsumerReplication<ID, DTO extends AbstractDto<ID>>
     public void listen(final List<ConsumerRecord<ID, GenericRecord>> records) {
         records.stream()
                 .map(this::map)
-                .forEach(replication -> replication.execute(service));
+                .forEach(this::execute);
     }
 
     @Override
     protected final Replication<ID, DTO> map(final GenericRecord record) {
-        try {
-            final TransportableReplication transportableReplication = createTransportableReplication(record);
-            final DTO dto = objectMapper.readValue(transportableReplication.getDtoJson(), dtoType);
-            return transportableReplication.getType().createReplication(dto);
-        } catch (final JsonProcessingException cause) {
-            throw new ReplicationConsumingException(cause);
-        }
+        final DTO dto = getDto(record);
+        return getReplicationType(record).createReplication(dto);
     }
 
-    private TransportableReplication createTransportableReplication(final GenericRecord record) {
-        return new TransportableReplication(getReplicationType(record), getDtoJson(record));
+    private void execute(final Replication<ID, DTO> replication) {
+        replication.execute(service);
     }
 
     private ReplicationType getReplicationType(final GenericRecord record) {
         return valueOf(getString(record, type));
+    }
+
+    private DTO getDto(final GenericRecord record) {
+        try {
+            return objectMapper.readValue(getDtoJson(record), dtoType);
+        } catch (final JsonProcessingException cause) {
+            throw new ReplicationConsumingException(cause);
+        }
     }
 
     private String getDtoJson(final GenericRecord record) {
