044a4364f77118615914e7b9f212bb797b5733cb
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 11:48:22 +0300

    add ReplicationTypeTest

---
+++ src/test/java/by/aurorasoft/replicator/model/ReplicationTypeTest.java
@@ -0,0 +1,40 @@
+package by.aurorasoft.replicator.model;
+
+import by.aurorasoft.replicator.model.replication.DeleteReplication;
+import by.aurorasoft.replicator.model.replication.Replication;
+import by.aurorasoft.replicator.model.replication.SaveReplication;
+import by.aurorasoft.replicator.model.replication.UpdateReplication;
+import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
+import lombok.Value;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static by.aurorasoft.replicator.model.ReplicationType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class ReplicationTypeTest {
+    private static final TestDto GIVEN_DTO = new TestDto(255L);
+
+    @ParameterizedTest
+    @MethodSource("provideTypeAndExpected")
+    public void replicationShouldBeCreated(final ReplicationType givenType, final Replication<Long, TestDto> expected) {
+        final Replication<Long, TestDto> actual = givenType.createReplication(GIVEN_DTO);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideTypeAndExpected() {
+        return Stream.of(
+                Arguments.of(SAVE, new SaveReplication<>(GIVEN_DTO)),
+                Arguments.of(UPDATE, new UpdateReplication<>(GIVEN_DTO)),
+                Arguments.of(DELETE, new DeleteReplication<>(GIVEN_DTO))
+        );
+    }
+
+    @Value
+    private static class TestDto implements AbstractDto<Long> {
+        Long id;
+    }
+}
