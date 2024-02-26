768898548af0296ebd64ad2fcae4d26fbae5a4cf
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 10:24:31 +0300

    add TestConfiguration and refactor AbstractTest

---
+++ src/test/java/by/aurorasoft/replicator/base/TestConfiguration.java
@@ -0,0 +1,25 @@
+package by.aurorasoft.replicator.base;
+
+import by.aurorasoft.replicator.config.ReplicationConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import static org.mockito.Mockito.mock;
+
+@Configuration
+@Import(ReplicationConfig.class)
+public class TestConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return mock(KafkaAdmin.class);
+    }
+}
