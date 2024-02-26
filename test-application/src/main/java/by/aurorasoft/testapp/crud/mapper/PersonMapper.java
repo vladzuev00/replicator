db75a6514e2536b9689a1f12b84882b6d93cf762
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-26 15:17:38 +0300

    refactor pom.xml in library and add PersonMapper in test app

---
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/mapper/PersonMapper.java
@@ -0,0 +1,27 @@
+package by.aurorasoft.testapp.crud.mapper;
+
+import by.aurorasoft.testapp.crud.dto.Person;
+import by.aurorasoft.testapp.crud.entity.PersonEntity;
+import by.nhorushko.crudgeneric.v2.mapper.AbsMapperEntityDto;
+import jakarta.persistence.EntityManager;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class PersonMapper extends AbsMapperEntityDto<PersonEntity, Person> {
+
+    public PersonMapper(final ModelMapper modelMapper, final EntityManager entityManager) {
+        super(modelMapper, entityManager, PersonEntity.class, Person.class);
+    }
+
+    @Override
+    protected Person create(final PersonEntity entity) {
+        return new Person(
+                entity.getId(),
+                entity.getName(),
+                entity.getSurname(),
+                entity.getPatronymic(),
+                entity.getBirthDate()
+        );
+    }
+}
