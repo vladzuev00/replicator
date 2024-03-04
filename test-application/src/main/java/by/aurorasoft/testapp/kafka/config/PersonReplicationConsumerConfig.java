b41bd56590409ee9e96edde16ac223c639ff77b8
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-04 17:06:12 +0300

    add ReplicationConsumingConfig

--- test-application/src/main/java/by/aurorasoft/testapp/kafka/config/PersonReplicationConsumerConfig.java
+++ test-application/src/main/java/by/aurorasoft/testapp/kafka/config/PersonReplicationConsumerConfig.java
@@ -1,13 +1,13 @@
 package by.aurorasoft.testapp.kafka.config;
 
-import by.aurorasoft.replicator.consumer.KafkakaReplicationConsumerStarter;
+import by.aurorasoft.replicator.consumer.KafkaReplicationConsumerConfig;
 import by.aurorasoft.testapp.crud.dto.ReplicatedPerson;
 import by.aurorasoft.testapp.crud.service.ReplicatedPersonService;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.springframework.stereotype.Component;
 
 @Component
-public final class PersonReplicationConsumerConfig extends KafkakaReplicationConsumerStarter<Long, ReplicatedPerson> {
+public final class PersonReplicationConsumerConfig extends KafkaReplicationConsumerConfig<Long, ReplicatedPerson> {
 
     public PersonReplicationConsumerConfig(ReplicatedPersonService service) {
         super("sync-person-group", "sync-person", new LongDeserializer(), ReplicatedPerson.class, service);
---
+++ test-application/src/main/java/by/aurorasoft/testapp/kafka/config/ReplicationConsumingConfig.java
@@ -0,0 +1,24 @@
+package by.aurorasoft.testapp.kafka.config;
+
+import by.aurorasoft.replicator.consumer.KafkaReplicationConsumerConfig;
+import by.aurorasoft.testapp.crud.dto.ReplicatedPerson;
+import by.aurorasoft.testapp.crud.service.ReplicatedPersonService;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReplicationConsumingConfig {
+
+    @Value("${kafka.topic.sync-person.group-id}")
+    private String groupId;
+
+    @Value("${kafka.topic.sync-person.name}")
+    private String topic;
+
+    @Bean
+    public KafkaReplicationConsumerConfig<Long, ReplicatedPerson> consumerConfig(final ReplicatedPersonService service) {
+        return new KafkaReplicationConsumerConfig<>(groupId, topic, new LongDeserializer(), ReplicatedPerson.class, service);
+    }
+}
