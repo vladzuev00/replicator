8841370ef97d2caf6cdb5dfba68d09dc3e845ba9
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 10:56:23 +0300

    refactor Replication

--- library/src/main/java/by/aurorasoft/replicator/model/replication/Replication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/replication/Replication.java
@@ -3,7 +3,20 @@ package by.aurorasoft.replicator.model.replication;
 import by.aurorasoft.replicator.model.ReplicationType;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.CLASS;
+
+@JsonTypeInfo(use = CLASS, property = "@class")
+@JsonSubTypes(
+        {
+                @Type(value = SaveReplication.class, name = "SaveReplication"),
+                @Type(value = UpdateReplication.class, name = "UpdateReplication"),
+                @Type(value = DeleteReplication.class, name = "DeleteReplication"),
+        }
+)
 public interface Replication<ID, DTO extends AbstractDto<ID>> {
     ID getEntityId();
 
