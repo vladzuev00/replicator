b41c1647bd1d0a2efed9f1f8800a12d0ea6d2ff3
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 10:42:31 +0300

    refactor DeleteReplication

--- library/src/main/java/by/aurorasoft/replicator/model/replication/DeleteReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/replication/DeleteReplication.java
@@ -3,22 +3,26 @@ package by.aurorasoft.replicator.model.replication;
 import by.aurorasoft.replicator.model.ReplicationType;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
+import lombok.RequiredArgsConstructor;
 
 import static by.aurorasoft.replicator.model.ReplicationType.DELETE;
 
-public final class DeleteReplication<ID, DTO extends AbstractDto<ID>> extends Replication<ID, DTO> {
+@RequiredArgsConstructor
+public final class DeleteReplication<ID, DTO extends AbstractDto<ID>> implements Replication<ID, DTO> {
+    private final ID id;
 
-    public DeleteReplication(final DTO dto) {
-        super(dto);
+    @Override
+    public ID getEntityId() {
+        return id;
+    }
+
+    @Override
+    public void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service) {
+        service.delete(id);
     }
 
     @Override
     public ReplicationType getType() {
         return DELETE;
-    }
-
-    @Override
-    protected void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service, final DTO dto) {
-        service.delete(dto.getId());
     }
 }
