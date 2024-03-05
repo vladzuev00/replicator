cf7f026f7f89da92c2863b9b4c32ba0f8dd5b934
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 10:44:56 +0300

    refactor SaveReplication

--- library/src/main/java/by/aurorasoft/replicator/model/replication/SaveReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/replication/SaveReplication.java
@@ -3,22 +3,26 @@ package by.aurorasoft.replicator.model.replication;
 import by.aurorasoft.replicator.model.ReplicationType;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
+import lombok.RequiredArgsConstructor;
 
 import static by.aurorasoft.replicator.model.ReplicationType.SAVE;
 
-public final class SaveReplication<ID, DTO extends AbstractDto<ID>> extends Replication<ID, DTO> {
+@RequiredArgsConstructor
+public final class SaveReplication<ID, DTO extends AbstractDto<ID>> implements Replication<ID, DTO> {
+    private final DTO dto;
 
-    public SaveReplication(final DTO dto) {
-        super(dto);
+    @Override
+    public ID getEntityId() {
+        return dto.getId();
+    }
+
+    @Override
+    public void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service) {
+        service.save(dto);
     }
 
     @Override
     public ReplicationType getType() {
         return SAVE;
-    }
-
-    @Override
-    protected void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service, final DTO dto) {
-        service.save(dto);
     }
 }
