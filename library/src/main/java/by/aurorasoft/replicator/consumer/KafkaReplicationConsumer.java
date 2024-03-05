e41daea25bd40415e29a9bd1c85a856b989d0f38
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 14:19:08 +0300

    refactor KafkaReplicationConsumer

--- library/src/main/java/by/aurorasoft/replicator/consumer/KafkaReplicationConsumer.java
+++ library/src/main/java/by/aurorasoft/replicator/consumer/KafkaReplicationConsumer.java
@@ -4,8 +4,6 @@ import by.aurorasoft.kafka.consumer.KafkaConsumerAbstract;
 import by.aurorasoft.replicator.model.Replication;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -14,8 +12,6 @@ import org.apache.kafka.common.serialization.Deserializer;
 @RequiredArgsConstructor
 public final class KafkaReplicationConsumer<ID, DTO extends AbstractDto<ID>> extends KafkaConsumerAbstract<ID, Replication<ID, DTO>> {
     private final AbsServiceCRUD<ID, ?, DTO, ?> service;
-    private final ObjectMapper objectMapper;
-    private final Class<DTO> dtoType;
 
     @Getter
     private final String groupId;
@@ -28,38 +24,6 @@ public final class KafkaReplicationConsumer<ID, DTO extends AbstractDto<ID>> ext
 
     @Override
     public void listen(final ConsumerRecord<ID, Replication<ID, DTO>> record) {
-//        final TransportableReplication replication = record.value();
-//        final DTO dto = deserializeDto(replication.getDtoJson());
-//        replication.getType().createReplication(dto).execute(service);
-    }
-
-    private DTO deserializeDto(final String dtoJson) {
-        try {
-            return objectMapper.readValue(dtoJson, dtoType);
-        } catch (final JsonProcessingException cause) {
-            throw new ReplicationConsumingException(cause);
-        }
-    }
-
-    static final class ReplicationConsumingException extends RuntimeException {
-
-        @SuppressWarnings("unused")
-        public ReplicationConsumingException() {
-
-        }
-
-        @SuppressWarnings("unused")
-        public ReplicationConsumingException(final String description) {
-            super(description);
-        }
-
-        public ReplicationConsumingException(final Exception cause) {
-            super(cause);
-        }
-
-        @SuppressWarnings("unused")
-        public ReplicationConsumingException(final String description, final Exception cause) {
-            super(description, cause);
-        }
+        record.value().execute(service);
     }
 }
