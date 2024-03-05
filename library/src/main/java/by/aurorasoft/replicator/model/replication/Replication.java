f622052a1d964afd00908e0765e8bad51b3205ea
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 10:41:41 +0300

    refactor Replication

--- library/src/main/java/by/aurorasoft/replicator/model/replication/Replication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/replication/Replication.java
@@ -3,27 +3,11 @@ package by.aurorasoft.replicator.model.replication;
 import by.aurorasoft.replicator.model.ReplicationType;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
-@RequiredArgsConstructor
-@Getter
-@EqualsAndHashCode
-@ToString
-public abstract class Replication<ID, DTO extends AbstractDto<ID>> {
-    private final DTO dto;
+public interface Replication<ID, DTO extends AbstractDto<ID>> {
+    ID getEntityId();
 
-    public final ID getEntityId() {
-        return dto.getId();
-    }
+    void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service);
 
-    public final void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service) {
-        execute(service, dto);
-    }
-
-    public abstract ReplicationType getType();
-
-    protected abstract void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service, final DTO dto);
+    ReplicationType getType();
 }
