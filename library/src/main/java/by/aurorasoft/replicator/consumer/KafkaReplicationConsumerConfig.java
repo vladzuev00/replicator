e5f73a5e2e852b7d80df60dd0b6048b2eb5e0382
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 17:26:37 +0300

    refactor KafkaReplicationConsumerConfig

--- library/src/main/java/by/aurorasoft/replicator/consumer/KafkaReplicationConsumerConfig.java
+++ library/src/main/java/by/aurorasoft/replicator/consumer/KafkaReplicationConsumerConfig.java
@@ -1,7 +1,9 @@
 package by.aurorasoft.replicator.consumer;
 
+import by.aurorasoft.replicator.model.Replication;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.serialization.Deserializer;
@@ -13,4 +15,5 @@ public final class KafkaReplicationConsumerConfig<ID, DTO extends AbstractDto<ID
     private final String topic;
     private final Deserializer<ID> idDeserializer;
     private final AbsServiceCRUD<ID, ?, DTO, ?> service;
+    private final TypeReference<Replication<ID, DTO>> replicationTypeReference;
 }
