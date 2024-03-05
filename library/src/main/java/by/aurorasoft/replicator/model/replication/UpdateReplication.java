0541a3b5fd5a6ecb109a3ed8328898eaf0c83bf8
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 10:46:02 +0300

    refactor UpdateReplication

--- library/src/main/java/by/aurorasoft/replicator/model/replication/UpdateReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/replication/UpdateReplication.java
@@ -3,22 +3,26 @@ package by.aurorasoft.replicator.model.replication;
 import by.aurorasoft.replicator.model.ReplicationType;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
+import lombok.RequiredArgsConstructor;
 
 import static by.aurorasoft.replicator.model.ReplicationType.UPDATE;
 
-public final class UpdateReplication<ID, DTO extends AbstractDto<ID>> extends Replication<ID, DTO> {
+@RequiredArgsConstructor
+public final class UpdateReplication<ID, DTO extends AbstractDto<ID>> implements Replication<ID, DTO> {
+    private final DTO dto;
 
-    public UpdateReplication(final DTO dto) {
-        super(dto);
+    @Override
+    public ID getEntityId() {
+        return dto.getId();
+    }
+
+    @Override
+    public void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service) {
+        service.update(dto);
     }
 
     @Override
     public ReplicationType getType() {
         return UPDATE;
-    }
-
-    @Override
-    protected void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service, final DTO dto) {
-        service.update(dto);
     }
 }
