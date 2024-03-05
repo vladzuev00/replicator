fe3eca2d02d7ecee8535ca2d326a596e110469d6
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-05 12:21:00 +0300

    refactor UpdateReplication

--- library/src/main/java/by/aurorasoft/replicator/model/replication/UpdateReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/replication/UpdateReplication.java
@@ -2,13 +2,26 @@ package by.aurorasoft.replicator.model.replication;
 
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
-@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
 public final class UpdateReplication<ID, DTO extends AbstractDto<ID>> implements Replication<ID, DTO> {
     private final DTO dto;
 
+    @JsonCreator
+    public UpdateReplication(@JsonProperty("dto") final DTO dto) {
+        this.dto = dto;
+    }
+
     @Override
+    @JsonIgnore
     public ID getEntityId() {
         return dto.getId();
     }
