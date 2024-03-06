fbfc4e4ea21bb2eceb803862ab4a4b288e41aade
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-06 17:48:05 +0300

    refactor ReplicationConsumerContainerFactoryTest

--- library/src/test/java/by/aurorasoft/replicator/factory/ReplicationConsumerContainerFactoryTest.java
+++ library/src/test/java/by/aurorasoft/replicator/factory/ReplicationConsumerContainerFactoryTest.java
@@ -11,15 +11,25 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.kafka.config.KafkaListenerEndpoint;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.MessageListenerContainer;
 
+import java.util.Map;
+
+import static by.aurorasoft.replicator.util.ReflectionUtil.getFieldValue;
 import static java.util.Collections.singletonList;
+import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class ReplicationConsumerContainerFactoryTest {
+    private static final String FIELD_NAME_CONSUMER_FACTORY = "consumerFactory";
+
     private static final String GIVEN_BOOTSTRAP_ADDRESS = "127.0.0.1:9092";
     private static final String GIVEN_TOPIC = "sync-dto";
     private static final int GIVEN_CONCURRENCY = 1;
@@ -46,10 +56,10 @@ public final class ReplicationConsumerContainerFactoryTest {
     public void containerShouldBeCreated() {
         final ReplicationConsumer<Long, TestDto> givenConsumer = mock(ReplicationConsumer.class);
 
-        final Deserializer<Long> givenDeserializer = mock(Deserializer.class);
+        final Deserializer<Long> givenIdDeserializer = mock(Deserializer.class);
         final String givenGroupId = "test-dto-group";
         final ReplicationConsumerConfig<Long, TestDto> givenConfig = createConsumerConfig(
-                givenDeserializer,
+                givenIdDeserializer,
                 givenGroupId
         );
         when(givenConsumer.getConfig()).thenReturn(givenConfig);
@@ -61,8 +71,22 @@ public final class ReplicationConsumerContainerFactoryTest {
         when(mockedReplicationDeserializerFactory.create(givenConfig)).thenReturn(givenReplicationDeserializer);
 
         final MessageListenerContainer actual = factory.create(givenConsumer);
+        final ConsumerFactory<Long, TestDto> actualConsumerFactory = getConsumerFactory(actual);
 
-        throw new RuntimeException();
+        final Map<String, Object> actualConsumerFactoryConfigsByNames = actualConsumerFactory
+                .getConfigurationProperties();
+        final Map<String, Object> expectedConsumerFactoryConfigsByNames = Map.of(
+                BOOTSTRAP_SERVERS_CONFIG, GIVEN_BOOTSTRAP_ADDRESS,
+                GROUP_ID_CONFIG, givenGroupId
+        );
+        assertEquals(expectedConsumerFactoryConfigsByNames, actualConsumerFactoryConfigsByNames);
+
+        final Deserializer<Long> actualKeyDeserializer = actualConsumerFactory.getKeyDeserializer();
+        assertSame(givenIdDeserializer, actualKeyDeserializer);
+
+        final Deserializer<TestDto> actualValueDeserializer = actualConsumerFactory.getValueDeserializer();
+        //noinspection AssertBetweenInconvertibleTypes
+        assertSame(givenReplicationDeserializer, actualValueDeserializer);
     }
 
     @SuppressWarnings("SameParameterValue")
@@ -79,5 +103,10 @@ public final class ReplicationConsumerContainerFactoryTest {
         when(endpoint.getTopics()).thenReturn(singletonList(GIVEN_TOPIC));
         when(endpoint.getConcurrency()).thenReturn(GIVEN_CONCURRENCY);
         return endpoint;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static ConsumerFactory<Long, TestDto> getConsumerFactory(final MessageListenerContainer container) {
+        return getFieldValue(container, FIELD_NAME_CONSUMER_FACTORY, ConsumerFactory.class);
     }
 }
