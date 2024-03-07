7c57f9f8006a734877f045a3fb5816b12f889240
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-07 10:23:45 +0300

    remove ReplicationConsumingStarter

--- library/src/main/java/by/aurorasoft/replicator/consuming/ReplicationConsumingStarter.java
+++ library/src/main/java/by/aurorasoft/replicator/consuming/ReplicationConsumingStarter.java
@@ -1,7 +1,6 @@
 package by.aurorasoft.replicator.consuming;
 
 import by.aurorasoft.replicator.consuming.consumer.ReplicationConsumer;
-import by.aurorasoft.replicator.consuming.consumer.ReplicationConsumerConfig;
 import by.aurorasoft.replicator.consuming.consumer.ReplicationConsumerStarter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -13,13 +12,11 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public final class ReplicationConsumingStarter {
-    private final List<ReplicationConsumerConfig<?, ?>> consumerConfigs;
+    private final List<ReplicationConsumer<?, ?>> consumers;
     private final ReplicationConsumerStarter consumerStarter;
 
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
-        consumerConfigs.stream()
-                .map(ReplicationConsumer::new)
-                .forEach(consumerStarter::start);
+        consumers.forEach(consumerStarter::start);
     }
 }
