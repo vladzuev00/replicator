ba440835ed065d8a19aa6921c6427cdd7a66faf4
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-07 11:13:02 +0300

    refactor ReplicationConsumingStarterTest

--- library/src/test/java/by/aurorasoft/replicator/consuming/ReplicationConsumingStarterTest.java
+++ library/src/test/java/by/aurorasoft/replicator/consuming/ReplicationConsumingStarterTest.java
@@ -1,8 +1,8 @@
 package by.aurorasoft.replicator.consuming;
 
 import by.aurorasoft.replicator.consuming.consumer.ReplicationConsumer;
-import by.aurorasoft.replicator.consuming.consumer.ReplicationConsumerConfig;
 import by.aurorasoft.replicator.consuming.consumer.ReplicationConsumerStarter;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -12,50 +12,47 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-import static java.util.stream.IntStream.range;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class ReplicationConsumingStarterTest {
 
     @Mock
+    private ReplicationConsumer<?, ?> firstMockedConsumer;
+
+    @Mock
+    private ReplicationConsumer<?, ?> secondMockedConsumer;
+
+    @Mock
     private ReplicationConsumerStarter mockedConsumerStarter;
+
+    private ReplicationConsumingStarter starter;
 
     @Captor
     private ArgumentCaptor<ReplicationConsumer<?, ?>> consumerArgumentCaptor;
 
+    @Before
+    public void initializeStarter() {
+        starter = new ReplicationConsumingStarter(
+                List.of(firstMockedConsumer, secondMockedConsumer),
+                mockedConsumerStarter
+        );
+    }
+
     @Test
     public void consumersShouldBeStarted() {
-        final ReplicationConsumerConfig<?, ?> firstGivenConfig = mock(ReplicationConsumerConfig.class);
-        final ReplicationConsumerConfig<?, ?> secondGivenConfig = mock(ReplicationConsumerConfig.class);
-        final ReplicationConsumingStarter givenStarter = createStarter(firstGivenConfig, secondGivenConfig);
-
-        givenStarter.start();
+        starter.start();
 
         verify(mockedConsumerStarter, times(2)).start(consumerArgumentCaptor.capture());
 
         final List<ReplicationConsumer<?, ?>> actualStartedConsumers = consumerArgumentCaptor.getAllValues();
         final List<ReplicationConsumer<?, ?>> expectedStartedConsumers = List.of(
-                new ReplicationConsumer<>(firstGivenConfig),
-                new ReplicationConsumer<>(secondGivenConfig)
+                firstMockedConsumer,
+                secondMockedConsumer
         );
-        checkEquals(expectedStartedConsumers, actualStartedConsumers);
-    }
-
-    private ReplicationConsumingStarter createStarter(final ReplicationConsumerConfig<?, ?>... consumerConfigs) {
-        return new ReplicationConsumingStarter(List.of(consumerConfigs), mockedConsumerStarter);
-    }
-
-    private static void checkEquals(final List<ReplicationConsumer<?, ?>> expected,
-                                    final List<ReplicationConsumer<?, ?>> actual) {
-        assertEquals(expected.size(), actual.size());
-        range(0, expected.size()).forEach(i -> checkEquals(expected.get(i), actual.get(i)));
-    }
-
-    private static void checkEquals(final ReplicationConsumer<?, ?> expected, final ReplicationConsumer<?, ?> actual) {
-        assertSame(expected.getConfig(), actual.getConfig());
+        assertEquals(expectedStartedConsumers, actualStartedConsumers);
     }
 }
 
