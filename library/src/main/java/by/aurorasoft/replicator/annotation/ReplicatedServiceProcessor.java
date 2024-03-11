cde5dbf53d3327f0e80d132838240610a1e29abc
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-11 14:45:16 +0300

    refactor ReplicatedServiceProcessor

--- library/src/main/java/by/aurorasoft/replicator/annotation/ReplicatedServiceProcessor.java
+++ library/src/main/java/by/aurorasoft/replicator/annotation/ReplicatedServiceProcessor.java
@@ -1,30 +1,35 @@
 package by.aurorasoft.replicator.annotation;
 
 import by.nhorushko.crudgeneric.v2.service.AbsServiceRUD;
+import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import java.lang.annotation.Annotation;
 import java.util.Set;
 import java.util.stream.Stream;
 
 import static javax.lang.model.SourceVersion.latestSupported;
 import static javax.tools.Diagnostic.Kind.ERROR;
-import static org.checkerframework.javacutil.ElementUtils.getAllSupertypes;
 
-//@AutoService(Processor.class)
+@AutoService(Processor.class)
 public final class ReplicatedServiceProcessor extends AbstractProcessor {
     private static final Class<? extends Annotation> ANNOTATION = ReplicatedService.class;
+    private static final Class<?> RUD_SERVICE_CLASS = AbsServiceRUD.class;
 
     @Override
     public boolean process(final Set<? extends TypeElement> handledAnnotations, final RoundEnvironment env) {
         handledAnnotations.stream()
                 .flatMap(annotation -> getAnnotatedElements(annotation, env))
-                .filter(element -> !isRUDServiceInheritor(element))
+                .filter(element -> !isRUDService(element))
                 .forEach(this::alertWrongAnnotating);
         return true;
     }
@@ -44,26 +49,38 @@ public final class ReplicatedServiceProcessor extends AbstractProcessor {
         return env.getElementsAnnotatedWith(annotation).stream();
     }
 
-    private boolean isRUDServiceInheritor(final Element element) {
-        return (element instanceof final TypeElement typeElement) && isRUDServiceInheritor(typeElement);
+    private boolean isRUDService(final Element element) {
+        return getTypeUtils().isAssignable(element.asType(), getRawRUDServiceType());
     }
 
-    private boolean isRUDServiceInheritor(final TypeElement element) {
-        return getAllSupertypes(element, processingEnv).stream().anyMatch(ReplicatedServiceProcessor::isRUDService);
+    private TypeMirror getRawRUDServiceType() {
+        return getTypeUtils().erasure(getRUDServiceType());
     }
 
-    private static boolean isRUDService(final TypeElement element) {
-        return element.getQualifiedName().contentEquals(AbsServiceRUD.class.getName());
+    private TypeMirror getRUDServiceType() {
+        return getElementUtils().getTypeElement(RUD_SERVICE_CLASS.getName()).asType();
     }
 
     private void alertWrongAnnotating(final Element element) {
-        processingEnv.getMessager().printMessage(ERROR, getWrongAnnotatingMessage(), element);
+        getMessager().printMessage(ERROR, getWrongAnnotatingMessage(), element);
     }
 
     private String getWrongAnnotatingMessage() {
-        return "Annotation '%s' can be applied only for subclasses of '%s'".formatted(
+        return "Annotation '@%s' can be applied only for subclasses of '%s'".formatted(
                 ANNOTATION.getSimpleName(),
-                AbsServiceRUD.class
+                RUD_SERVICE_CLASS
         );
+    }
+
+    private Types getTypeUtils() {
+        return processingEnv.getTypeUtils();
+    }
+
+    private Elements getElementUtils() {
+        return processingEnv.getElementUtils();
+    }
+
+    private Messager getMessager() {
+        return processingEnv.getMessager();
     }
 }
