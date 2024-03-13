29fc4558ae9075bbe7e5823730e349d020f44d9b
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-13 16:49:27 +0300

    refactor DeleteConsumedReplication and add DeleteConsumedReplicationTest

--- library/src/main/java/by/aurorasoft/replicator/model/consumed/DeleteConsumedReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/consumed/DeleteConsumedReplication.java
@@ -1,15 +1,19 @@
 package by.aurorasoft.replicator.model.consumed;
 
 import by.nhorushko.crudgeneric.v2.domain.AbstractEntity;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import static by.aurorasoft.replicator.util.TransportNameUtil.DELETE_ENTITY_ID;
 
 @Value
 public class DeleteConsumedReplication<ID, E extends AbstractEntity<ID>> implements ConsumedReplication<ID, E> {
     ID entityId;
 
-    public DeleteConsumedReplication(@JsonProperty("entityId") final ID entityId) {
+    @JsonCreator
+    public DeleteConsumedReplication(@JsonProperty(DELETE_ENTITY_ID) final ID entityId) {
         this.entityId = entityId;
     }
 
---
+++ library/src/test/java/by/aurorasoft/replicator/model/consumed/DeleteConsumedReplicationTest.java
@@ -0,0 +1,24 @@
+package by.aurorasoft.replicator.model.consumed;
+
+import by.aurorasoft.replicator.base.entity.TestEntity;
+import org.junit.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.*;
+
+public final class DeleteConsumedReplicationTest {
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void replicationShouldBeExecuted() {
+        final Long givenEntityId = 255L;
+        final var givenReplication = new DeleteConsumedReplication<Long, TestEntity>(givenEntityId);
+
+        final JpaRepository<TestEntity, Long> givenRepository = mock(JpaRepository.class);
+
+        givenReplication.execute(givenRepository);
+
+        verify(givenRepository, times(1)).deleteById(same(givenEntityId));
+    }
+}
