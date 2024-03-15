1a3a4ac897d3e42b1d2496fa34ae02c6abcd075a
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-15 16:26:41 +0300

    refactor SaveProducedReplication

--- library/src/main/java/by/aurorasoft/replicator/model/produced/SaveProducedReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/produced/SaveProducedReplication.java
@@ -3,13 +3,17 @@ package by.aurorasoft.replicator.model.produced;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import static by.aurorasoft.replicator.util.TransportNameUtil.BODY_NAME;
 
 @RequiredArgsConstructor
 @Getter
+@EqualsAndHashCode
+@ToString
 public final class SaveProducedReplication<ID, DTO extends AbstractDto<ID>> implements ProducedReplication<ID> {
 
     @JsonProperty(BODY_NAME)
