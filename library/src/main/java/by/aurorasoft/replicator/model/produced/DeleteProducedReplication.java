244213866ef7cf4e3752168b0e244f2680cb9a4d
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-15 16:27:04 +0300

    refactor DeleteProducedReplication

--- library/src/main/java/by/aurorasoft/replicator/model/produced/DeleteProducedReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/produced/DeleteProducedReplication.java
@@ -1,13 +1,17 @@
 package by.aurorasoft.replicator.model.produced;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import static by.aurorasoft.replicator.util.TransportNameUtil.ENTITY_ID_NAME;
 
 @RequiredArgsConstructor
 @Getter
+@EqualsAndHashCode
+@ToString
 public final class DeleteProducedReplication<ID> implements ProducedReplication<ID> {
 
     @JsonProperty(ENTITY_ID_NAME)
