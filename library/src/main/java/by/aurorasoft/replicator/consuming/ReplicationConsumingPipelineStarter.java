8e9733a355e4c26c03e7654cc91018ca51339cda
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-15 15:40:51 +0300

    refactor ReplicationConsumingPipelineStarter

--- library/src/main/java/by/aurorasoft/replicator/consuming/ReplicationConsumingPipelineStarter.java
+++ library/src/main/java/by/aurorasoft/replicator/consuming/ReplicationConsumingPipelineStarter.java
@@ -9,9 +9,10 @@ import static org.apache.kafka.streams.kstream.Consumed.with;
 @Component
 public final class ReplicationConsumingPipelineStarter {
 
-    public <ID, E extends AbstractEntity<ID>> void start(final ReplicationConsumingPipelineConfig<ID, E> pipeline, final StreamsBuilder streamsBuilder) {
+    public <ID, E extends AbstractEntity<ID>> void start(final ReplicationConsumingPipelineConfig<ID, E> config,
+                                                         final StreamsBuilder streamsBuilder) {
         streamsBuilder
-                .stream(pipeline.getTopic(), with(pipeline.getIdSerde(), pipeline.getReplicationSerde()))
-                .foreach((id, replication) -> replication.execute(pipeline.getRepository()));
+                .stream(config.getTopic(), with(config.getIdSerde(), config.getReplicationSerde()))
+                .foreach((id, replication) -> replication.execute(config.getRepository()));
     }
 }
