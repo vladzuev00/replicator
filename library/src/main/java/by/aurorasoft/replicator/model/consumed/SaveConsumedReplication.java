c21f5f592a12d9bbf97613fca973af45cc84bb93
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-15 16:25:42 +0300

    refactor SaveConsumedReplication

--- library/src/main/java/by/aurorasoft/replicator/model/consumed/SaveConsumedReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/consumed/SaveConsumedReplication.java
@@ -3,12 +3,16 @@ package by.aurorasoft.replicator.model.consumed;
 import by.nhorushko.crudgeneric.v2.domain.AbstractEntity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import static by.aurorasoft.replicator.util.TransportNameUtil.BODY_NAME;
 
 @Getter
+@EqualsAndHashCode
+@ToString
 public final class SaveConsumedReplication<ID, E extends AbstractEntity<ID>> implements ConsumedReplication<ID, E> {
     private final E entity;
 
