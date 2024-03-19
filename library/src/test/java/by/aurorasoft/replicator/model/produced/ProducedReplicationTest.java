873a13adf62d9c32f70c28c11e05b1b2a0d9451e
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-19 22:09:45 +0300

    refactor ProducedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/produced/ProducedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/produced/ProducedReplicationTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static java.util.UUID.fromString;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 public final class ProducedReplicationTest {
@@ -19,36 +18,27 @@ public final class ProducedReplicationTest {
     public void replicationShouldBeSerializedToJson(final ProducedReplication<Long> givenReplication,
                                                     final String expected)
             throws Exception {
-//        final String actual = objectMapper.writeValueAsString(givenReplication);
-//        assertEquals(expected, actual, true);
-        throw new RuntimeException();
+        final String actual = objectMapper.writeValueAsString(givenReplication);
+        assertEquals(expected, actual, true);
     }
 
     private static Stream<Arguments> provideReplicationAndExpectedJson() {
         return Stream.of(
                 Arguments.of(
-//                        new SaveProducedReplication<>(
-//                                fromString("e52232e1-0ded-4587-999f-4dd135a4a94f"),
-//                                new TestDto(255L)
-//                        ),
+                        new SaveProducedReplication<>(new TestDto(255L)),
                         """
                                 {
                                   "type": "save",
-                                  "uuid": "e52232e1-0ded-4587-999f-4dd135a4a94f",
                                   "body": {
                                     "id": 255
                                   }
                                 }"""
                 ),
                 Arguments.of(
-//                        new DeleteProducedReplication<>(
-//                                fromString("e52232e2-0ded-4587-999f-4dd135a4a95f"),
-//                                255L
-//                        ),
+                        new DeleteProducedReplication<>(255L),
                         """
                                 {
                                   "type": "delete",
-                                  "uuid": "e52232e2-0ded-4587-999f-4dd135a4a95f",
                                   "entityId": 255
                                 }"""
                 )
