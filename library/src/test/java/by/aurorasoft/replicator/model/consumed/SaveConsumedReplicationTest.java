784fc40233418e53a8c1a25db107a0e15eb90317
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-19 21:00:00 +0300

    refactor SaveConsumedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/consumed/SaveConsumedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/consumed/SaveConsumedReplicationTest.java
@@ -4,7 +4,6 @@ import by.aurorasoft.replicator.base.entity.TestEntity;
 import org.junit.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import static java.util.UUID.randomUUID;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.*;
 
@@ -13,17 +12,13 @@ public final class SaveConsumedReplicationTest {
     @Test
     @SuppressWarnings("unchecked")
     public void replicationShouldBeExecuted() {
-//        final TestEntity givenEntity = new TestEntity(255L);
-//        final SaveConsumedReplication<Long, TestEntity> givenReplication = new SaveConsumedReplication<>(
-//                randomUUID(),
-//                givenEntity
-//        );
-//
-//        final JpaRepository<TestEntity, Long> givenRepository = mock(JpaRepository.class);
-//
-//        givenReplication.execute(givenRepository);
-//
-//        verify(givenRepository, times(1)).save(same(givenEntity));
-        throw new RuntimeException();
+        final TestEntity givenEntity = new TestEntity(255L);
+        final SaveConsumedReplication<Long, TestEntity> givenReplication = new SaveConsumedReplication<>(givenEntity);
+
+        final JpaRepository<TestEntity, Long> givenRepository = mock(JpaRepository.class);
+
+        givenReplication.execute(givenRepository);
+
+        verify(givenRepository, times(1)).save(same(givenEntity));
     }
 }
