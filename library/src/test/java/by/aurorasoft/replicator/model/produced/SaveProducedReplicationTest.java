4ed2906dfbecb564ea501f56a076fbbfbe0224b3
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-19 22:10:55 +0300

    refactor SaveProducedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/produced/SaveProducedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/produced/SaveProducedReplicationTest.java
@@ -3,18 +3,16 @@ package by.aurorasoft.replicator.model.produced;
 import by.aurorasoft.replicator.base.dto.TestDto;
 import org.junit.Test;
 
-import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertSame;
 
 public final class SaveProducedReplicationTest {
 
     @Test
     public void entityIdShouldBeGot() {
-//        final Long givenId = 255L;
-//        final var givenReplication = new SaveProducedReplication<>(randomUUID(), new TestDto(givenId));
-//
-//        final Long actual = givenReplication.getEntityId();
-//        assertSame(givenId, actual);
-        throw new RuntimeException();
+        final Long givenId = 255L;
+        final var givenReplication = new SaveProducedReplication<>(new TestDto(givenId));
+
+        final Long actual = givenReplication.getEntityId();
+        assertSame(givenId, actual);
     }
 }
