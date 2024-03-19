cf1c91dc065bd11f377c47c477a4abcb3aa24a82
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-19 22:38:22 +0300

    refactor TransportNameUtil

--- library/src/main/java/by/aurorasoft/replicator/util/TransportNameUtil.java
+++ library/src/main/java/by/aurorasoft/replicator/util/TransportNameUtil.java
@@ -7,8 +7,6 @@ public final class TransportNameUtil {
     public static final String TYPE_PROPERTY = "type";
     public static final String SAVE = "save";
     public static final String DELETE = "delete";
-
-    public static final String UUID_NAME = "uuid";
     public static final String BODY_NAME = "body";
     public static final String ENTITY_ID_NAME = "entityId";
 }
