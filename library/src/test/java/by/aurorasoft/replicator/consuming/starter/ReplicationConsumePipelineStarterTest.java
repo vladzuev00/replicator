0fc530add5ada973716accd2864f25fcb4606592
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-25 11:42:48 +0300

    refactor ReplicationConsumePipelineStarterTest

--- library/src/test/java/by/aurorasoft/replicator/consuming/starter/ReplicationConsumePipelineStarterTest.java
+++ library/src/test/java/by/aurorasoft/replicator/consuming/starter/ReplicationConsumePipelineStarterTest.java
@@ -2,53 +2,63 @@ package by.aurorasoft.replicator.consuming.starter;
 
 import by.aurorasoft.replicator.base.entity.TestEntity;
 import by.aurorasoft.replicator.consuming.pipeline.ReplicationConsumePipeline;
+import by.aurorasoft.replicator.consuming.serde.ReplicationSerde;
 import by.aurorasoft.replicator.model.consumed.ConsumedReplication;
-import by.aurorasoft.replicator.model.consumed.DeleteConsumedReplication;
-import by.aurorasoft.replicator.model.consumed.SaveConsumedReplication;
 import com.fasterxml.jackson.core.type.TypeReference;
-import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes.LongSerde;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.TestInputTopic;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.ForeachAction;
+import org.apache.kafka.streams.kstream.KStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import static by.aurorasoft.replicator.util.ReflectionUtil.getFieldValue;
 import static org.apache.kafka.common.serialization.Serdes.Long;
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.*;
 
-//TODO: realise
 @RunWith(MockitoJUnitRunner.class)
 public final class ReplicationConsumePipelineStarterTest {
     private static final String GIVEN_TOPIC = "sync-dto";
 
+    private static final String FIELD_NAME_KEY_SERDE = "keySerde";
+    private static final String FIELD_NAME_VALUE_SERDE = "valueSerde";
+
     private final ReplicationConsumePipelineStarter starter = new ReplicationConsumePipelineStarter();
+
+    @Mock
+    private StreamsBuilder mockedStreamsBuilder;
 
     @Mock
     private JpaRepository<TestEntity, Long> mockedRepository;
 
+    @Captor
+    private ArgumentCaptor<Consumed<?, ?>> consumedArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<ForeachAction<Long, ConsumedReplication<Long, TestEntity>>> foreachActionArgumentCaptor;
+
     @Test
-    public void pipelineShouldBeStartedAndReceiveReplications() {
+    @SuppressWarnings("unchecked")
+    public void pipelineShouldBeStarted() {
         final ReplicationConsumePipeline<Long, TestEntity> givenPipeline = createPipeline();
-        final StreamsBuilder givenStreamBuilder = new StreamsBuilder();
 
-        starter.start(givenPipeline, givenStreamBuilder);
+        final KStream<Long, ConsumedReplication<Long, TestEntity>> givenStream = mock(KStream.class);
+        when(mockedStreamsBuilder.stream(same(GIVEN_TOPIC), any(Consumed.class))).thenReturn(givenStream);
 
-        final Topology topology = givenStreamBuilder.build();
-        final TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology);
+        starter.start(givenPipeline, mockedStreamsBuilder);
 
-        final TestInputTopic<Long, ConsumedReplication<Long, TestEntity>> givenTopic = topologyTestDriver
-                .createInputTopic(GIVEN_TOPIC, new LongSerializer(), new JsonSerializer<>());
-
-        givenTopic.pipeInput(new SaveConsumedReplication<>(new TestEntity(255L)));
-        givenTopic.pipeInput(new DeleteConsumedReplication<>(255L));
-
-        Mockito.verify(mockedRepository, Mockito.times(1)).save(Mockito.eq(new TestEntity(255L)));
-        Mockito.verify(mockedRepository, Mockito.times(1)).deleteById(Mockito.eq(255L));
+        verifyConsumed(LongSerde.class, ReplicationSerde.class);
+        verifyForeachAction(givenStream);
     }
 
     private ReplicationConsumePipeline<Long, TestEntity> createPipeline() {
@@ -59,5 +69,36 @@ public final class ReplicationConsumePipelineStarterTest {
                 },
                 mockedRepository
         );
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private void verifyConsumed(final Class<?> idSerdeType, final Class<?> valueSerdeType) {
+        verify(mockedStreamsBuilder, times(1))
+                .stream(same(GIVEN_TOPIC), consumedArgumentCaptor.capture());
+        final Consumed<?, ?> actual = consumedArgumentCaptor.getValue();
+        assertSame(idSerdeType, getKeySerdeType(actual));
+        assertSame(valueSerdeType, getValueSerdeType(actual));
+    }
+
+    private static Class<?> getKeySerdeType(final Consumed<?, ?> consumed) {
+        return getSerdeType(consumed, FIELD_NAME_KEY_SERDE);
+    }
+
+    private static Class<?> getValueSerdeType(final Consumed<?, ?> consumed) {
+        return getSerdeType(consumed, FIELD_NAME_VALUE_SERDE);
+    }
+
+    private static Class<?> getSerdeType(final Consumed<?, ?> consumed, final String fieldName) {
+        return getFieldValue(consumed, fieldName, Serde.class).getClass();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void verifyForeachAction(final KStream<Long, ConsumedReplication<Long, TestEntity>> stream) {
+        verify(stream, times(1)).foreach(foreachActionArgumentCaptor.capture());
+        final ForeachAction<Long, ConsumedReplication<Long, TestEntity>> actual = foreachActionArgumentCaptor.getValue();
+        final Long givenId = 255L;
+        final ConsumedReplication<Long, TestEntity> givenReplication = mock(ConsumedReplication.class);
+        actual.apply(givenId, givenReplication);
+        verify(givenReplication, times(1)).execute(same(mockedRepository));
     }
 }
