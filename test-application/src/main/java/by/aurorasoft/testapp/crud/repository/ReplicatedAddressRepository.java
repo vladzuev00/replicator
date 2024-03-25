53e3259ad34aac47c1ce44437d07d4737704d08b
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-25 16:24:27 +0300

    add ReplicatedAddressRepository

---
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/repository/ReplicatedAddressRepository.java
@@ -0,0 +1,8 @@
+package by.aurorasoft.testapp.crud.repository;
+
+import by.aurorasoft.testapp.crud.entity.ReplicatedAddressEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplicatedAddressRepository extends JpaRepository<ReplicatedAddressEntity, Long> {
+
+}
