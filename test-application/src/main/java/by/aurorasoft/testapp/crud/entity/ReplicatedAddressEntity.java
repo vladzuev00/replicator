3a169feabdddd1449607986f482b9771546b7f8b
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-25 16:08:21 +0300

    add ReplicatedAddressEntity

---
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/entity/ReplicatedAddressEntity.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.testapp.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+@Entity
+@Table(name = "replicated_addresses")
+public class ReplicatedAddressEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+}
+
