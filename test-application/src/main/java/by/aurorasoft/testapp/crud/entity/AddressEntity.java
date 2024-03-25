5ae8ce38d21439589d98c4b6747a9bd49ced434b
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-25 16:03:03 +0300

    add AddressEntity

---
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/entity/AddressEntity.java
@@ -0,0 +1,28 @@
+package by.aurorasoft.testapp.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+@Entity
+@Table(name = "addresses")
+public class AddressEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+}
