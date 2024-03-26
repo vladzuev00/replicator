7b115cf6cd6cb19d2759b8034ac2324bb484cd12
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-26 17:38:08 +0300

    refactor ReplicationConsumeExceptionHandler

--- library/src/main/java/by/aurorasoft/replicator/consuming/exceptionhandler/ReplicationConsumeExceptionHandler.java
+++ library/src/main/java/by/aurorasoft/replicator/consuming/exceptionhandler/ReplicationConsumeExceptionHandler.java
@@ -4,19 +4,34 @@ import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.stereotype.Component;
 
-import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD;
+import java.sql.SQLException;
+import java.util.Objects;
 
-//TODO: refactor, possible receive ReplicationConsumeException
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCause;
+import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD;
+import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
+
+//TODO: refactor, possible receive ReplicationConsumeException, do only for replication consuming
 @Component
 public final class ReplicationConsumeExceptionHandler implements StreamsUncaughtExceptionHandler {
+    private static final String INVALID_FOREIGN_KEY_SQL_STATE = "23503";
 
+    //TODO: remove
     public ReplicationConsumeExceptionHandler(StreamsBuilderFactoryBean streamBuilderFactoryBean) {
         streamBuilderFactoryBean.setStreamsUncaughtExceptionHandler(this);
     }
 
     @Override
     public StreamThreadExceptionResponse handle(final Throwable exception) {
-        System.out.println("Uncaught exception: " + exception);
-        return REPLACE_THREAD;
+        System.out.println("Replication exception : " + exception);
+        return isPerhapsRelationNotDeliveredYet(exception) ? REPLACE_THREAD : SHUTDOWN_CLIENT;
+    }
+
+    private static boolean isPerhapsRelationNotDeliveredYet(final Throwable exception) {
+        return (getRootCause(exception) instanceof final SQLException sqlException) && isInvalidForeignKey(sqlException);
+    }
+
+    private static boolean isInvalidForeignKey(final SQLException exception) {
+        return Objects.equals(INVALID_FOREIGN_KEY_SQL_STATE, exception.getSQLState());
     }
 }
