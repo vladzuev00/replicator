5c19c7f72482733a934ed336ef3838db7b144ac2
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-29 15:57:25 +0300

    refactor ReplicationConsumeStarterTest

--- library/src/test/java/by/aurorasoft/replicator/consuming/starter/ReplicationConsumeStarterTest.java
+++ library/src/test/java/by/aurorasoft/replicator/consuming/starter/ReplicationConsumeStarterTest.java
@@ -1,66 +1,54 @@
-//package by.aurorasoft.replicator.consuming.starter;
-//
-//import by.aurorasoft.replicator.base.entity.TestEntity;
-//import by.aurorasoft.replicator.consuming.pipeline.ReplicationConsumePipeline;
-//import org.apache.kafka.streams.StreamsBuilder;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.Captor;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//import java.util.List;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.same;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public final class ReplicationConsumeStarterTest {
-//
-//    @Mock
-//    private ReplicationConsumePipeline<Long, TestEntity> firstMockedPipeline;
-//
-//    @Mock
-//    private ReplicationConsumePipeline<Long, TestEntity> secondMockedPipeline;
-//
-//    @Mock
-//    private ReplicationConsumePipelineStarter mockedPipelineStarter;
-//
-//    @Mock
-//    private StreamsBuilder mockedStreamsBuilder;
-//
-//    private ReplicationConsumeStarter starter;
-//
-//    @Captor
-//    private ArgumentCaptor<ReplicationConsumePipeline<Long, TestEntity>> pipelineArgumentCaptor;
-//
-//    @Before
-//    public void initializeStarter() {
-//        starter = new ReplicationConsumeStarter(
-//                List.of(firstMockedPipeline, secondMockedPipeline),
-//                mockedPipelineStarter,
-//                mockedStreamsBuilder
-//        );
-//    }
-//
-//    @Test
-//    public void consumingShouldBeStarted() {
-//        starter.start();
-//
-//        verify(mockedPipelineStarter, times(2)).start(
-//                pipelineArgumentCaptor.capture(),
-//                same(mockedStreamsBuilder)
-//        );
-//
-//        final List<ReplicationConsumePipeline<Long, TestEntity>> actual = pipelineArgumentCaptor.getAllValues();
-//        final List<ReplicationConsumePipeline<Long, TestEntity>> expected = List.of(
-//                firstMockedPipeline,
-//                secondMockedPipeline
-//        );
-//        assertEquals(expected, actual);
-//    }
-//}
+package by.aurorasoft.replicator.consuming.starter;
+
+import by.aurorasoft.replicator.base.entity.TestEntity;
+import by.aurorasoft.replicator.consuming.pipeline.ReplicationConsumePipeline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class ReplicationConsumeStarterTest {
+
+    @Mock
+    private ReplicationConsumePipeline<Long, TestEntity> firstMockedPipeline;
+
+    @Mock
+    private ReplicationConsumePipeline<Long, TestEntity> secondMockedPipeline;
+
+    @Mock
+    private ReplicationConsumePipelineStarter mockedPipelineStarter;
+
+    private ReplicationConsumeStarter starter;
+
+    @Captor
+    private ArgumentCaptor<ReplicationConsumePipeline<Long, TestEntity>> pipelineArgumentCaptor;
+
+    @Before
+    public void initializeStarter() {
+        starter = new ReplicationConsumeStarter(getGivenPipelines(), mockedPipelineStarter);
+    }
+
+    @Test
+    public void consumingShouldBeStarted() {
+        starter.start();
+
+        final List<ReplicationConsumePipeline<?, ?>> expected = getGivenPipelines();
+        verify(mockedPipelineStarter, times(expected.size())).start(pipelineArgumentCaptor.capture());
+        final List<ReplicationConsumePipeline<Long, TestEntity>> actual = pipelineArgumentCaptor.getAllValues();
+        assertEquals(expected, actual);
+    }
+
+    private List<ReplicationConsumePipeline<?, ?>> getGivenPipelines() {
+        return List.of(firstMockedPipeline, secondMockedPipeline);
+    }
+}
