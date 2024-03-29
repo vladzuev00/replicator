aed12a96208ea7fe5e39106c390de842cc6d9851
Vlad Zuev <vladzuev.00@mail.ru>
2024-03-29 13:22:22 +0300

    add ConsumeExceptionHandler and refactor SaveConsumedReplication

--- library/src/main/java/by/aurorasoft/replicator/model/consumed/SaveConsumedReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/model/consumed/SaveConsumedReplication.java
@@ -1,6 +1,5 @@
 package by.aurorasoft.replicator.model.consumed;
 
-import by.aurorasoft.replicator.exception.PerhapsRelationNotDeliveredYetException;
 import by.nhorushko.crudgeneric.v2.domain.AbstractEntity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,18 +8,13 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.SQLException;
-import java.util.Objects;
-
 import static by.aurorasoft.replicator.util.TransportNameUtil.BODY_NAME;
-import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCause;
 
+//TODO: refactor tests
 @Getter
 @EqualsAndHashCode
 @ToString
 public final class SaveConsumedReplication<ID, E extends AbstractEntity<ID>> implements ConsumedReplication<ID, E> {
-    private static final String INVALID_FOREIGN_KEY_SQL_STATE = "23503";
-
     private final E entity;
 
     @JsonCreator
@@ -30,46 +24,6 @@ public final class SaveConsumedReplication<ID, E extends AbstractEntity<ID>> imp
 
     @Override
     public void execute(final JpaRepository<E, ID> repository) {
-        try {
-            repository.save(entity);
-        } catch (final Exception exception) {
-            throw wrapFailedExecutionException(exception);
-        }
-    }
-
-    private static RuntimeException wrapFailedExecutionException(final Exception cause) {
-        return isPerhapsRelationNotDeliveredYet(cause)
-                ? new PerhapsRelationNotDeliveredYetException(cause)
-                : new SaveReplicationExecutionException(cause);
-    }
-
-    private static boolean isPerhapsRelationNotDeliveredYet(final Exception exception) {
-        return (getRootCause(exception) instanceof final SQLException sqlException) && isInvalidForeignKey(sqlException);
-    }
-
-    private static boolean isInvalidForeignKey(final SQLException exception) {
-        return Objects.equals(INVALID_FOREIGN_KEY_SQL_STATE, exception.getSQLState());
-    }
-
-    static final class SaveReplicationExecutionException extends RuntimeException {
-
-        @SuppressWarnings("unused")
-        public SaveReplicationExecutionException() {
-
-        }
-
-        @SuppressWarnings("unused")
-        public SaveReplicationExecutionException(final String description) {
-            super(description);
-        }
-
-        public SaveReplicationExecutionException(final Exception cause) {
-            super(cause);
-        }
-
-        @SuppressWarnings("unused")
-        public SaveReplicationExecutionException(final String description, final Exception cause) {
-            super(description, cause);
-        }
+        repository.save(entity);
     }
 }
