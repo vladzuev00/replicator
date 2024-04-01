a06f3295750216162d9a5c4c2b6ada1169bfaacc
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-01 15:14:13 +0300

    add data.sql

---
+++ test-application/src/test/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO addresses(id, country, city) VALUES(255, 'Russia', 'Moscow');
+INSERT INTO addresses(id, country, city) VALUES(256, 'America', 'Chicago');
+INSERT INTO addresses(id, country, city) VALUES(257, 'Austria', 'Vienna');
+INSERT INTO addresses(id, country, city) VALUES(258, 'Austria', 'Styria');
+INSERT INTO addresses(id, country, city) VALUES(259, 'Austria', 'Tyrol');
+INSERT INTO addresses(id, country, city) VALUES(260, 'Estonia', 'Tallinn');
+INSERT INTO addresses(id, country, city) VALUES(261, 'Estonia', 'Tartu');
+INSERT INTO addresses(id, country, city) VALUES(262, 'Estonia', 'Narva');
+
+INSERT INTO replicated_addresses(id, country, city) VALUES(255, 'Russia', 'Moscow');
+INSERT INTO replicated_addresses(id, country, city) VALUES(256, 'America', 'Chicago');
+INSERT INTO replicated_addresses(id, country, city) VALUES(257, 'Austria', 'Vienna');
+INSERT INTO replicated_addresses(id, country, city) VALUES(258, 'Austria', 'Styria');
+INSERT INTO replicated_addresses(id, country, city) VALUES(259, 'Austria', 'Tyrol');
+INSERT INTO replicated_addresses(id, country, city) VALUES(260, 'Estonia', 'Tallinn');
+INSERT INTO replicated_addresses(id, country, city) VALUES(261, 'Estonia', 'Tartu');
+INSERT INTO replicated_addresses(id, country, city) VALUES(262, 'Estonia', 'Narva');
+
+INSERT INTO persons(id, name, surname, patronymic, birth_date, address_id) VALUES(255, 'Vlad', 'Zuev', 'Sergeevich', '2000-02-18', 255);
+INSERT INTO persons(id, name, surname, patronymic, birth_date, address_id) VALUES(256, 'Vasilii', 'Borisovich', 'Dolzhikov', '1980-03-15', 255);
+INSERT INTO persons(id, name, surname, patronymic, birth_date, address_id) VALUES(257, 'Fedya', 'Yemelyanovich', 'Amelin', '1990-04-15', 255);
+INSERT INTO persons(id, name, surname, patronymic, birth_date, address_id) VALUES(258, 'Pashenka', 'Filippovich', 'Kornev', '1995-04-23', 256);
+INSERT INTO persons(id, name, surname, patronymic, birth_date, address_id) VALUES(259, 'Sasha', 'Gennadiyevich', 'Kuzmin', '1996-02-21', 257);
+
+INSERT INTO replicated_persons(id, name, surname, birth_date, address_id) VALUES(255, 'Vlad', 'Zuev', '2000-02-18', 255);
+INSERT INTO replicated_persons(id, name, surname, birth_date, address_id) VALUES(256, 'Vasilii', 'Borisovich', '1980-03-15', 255);
+INSERT INTO replicated_persons(id, name, surname, birth_date, address_id) VALUES(257, 'Fedya', 'Yemelyanovich', '1990-04-15', 255);
+INSERT INTO replicated_persons(id, name, surname, birth_date, address_id) VALUES(258, 'Pashenka', 'Filippovich', '1995-04-23', 256);
+INSERT INTO replicated_persons(id, name, surname, birth_date, address_id) VALUES(259, 'Sasha', 'Gennadiyevich', '1996-02-21', 257);
--- test-application/src/test/resources/sql-scripts/replication/it/insert-person-and-addresses.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO addresses(id, country, city) VALUES(255, 'Belarus', 'Minsk');
-INSERT INTO addresses(id, country, city) VALUES(256, 'America', 'Chicago');
-
-INSERT INTO replicated_addresses(id, country, city) VALUES(255, 'Belarus', 'Minsk');
-INSERT INTO replicated_addresses(id, country, city) VALUES(256, 'America', 'Chicago');
-
-INSERT INTO persons(id, name, surname, patronymic, birth_date, address_id) VALUES(255, 'Vlad', 'Zuev', 'Sergeevich', '2000-02-18', 255);
-
-INSERT INTO replicated_persons(id, name, surname, birth_date, address_id) VALUES(255, 'Vlad', 'Zuev', '2000-02-18', 255);
