0832dbceae8c8e1db9a0163f6df1753ca9ee5d81
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-01 17:39:27 +0300

    add PersonEntityUtil

---
+++ test-application/src/test/java/by/aurorasoft/testapp/util/PersonEntityUtil.java
@@ -0,0 +1,19 @@
+package by.aurorasoft.testapp.util;
+
+import by.aurorasoft.testapp.crud.entity.PersonEntity;
+import lombok.experimental.UtilityClass;
+
+import static org.junit.Assert.assertEquals;
+
+@UtilityClass
+public final class PersonEntityUtil {
+
+    public static void checkEquals(final PersonEntity expected, final PersonEntity actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getSurname(), actual.getSurname());
+        assertEquals(expected.getPatronymic(), actual.getPatronymic());
+        assertEquals(expected.getBirthDate(), actual.getBirthDate());
+        assertEquals(expected.getAddress().getId(), actual.getAddress().getId());
+    }
+}
