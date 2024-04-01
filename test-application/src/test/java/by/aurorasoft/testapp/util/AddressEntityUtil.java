3710810ce239feaa0d5ae9bb638d786daa2bfa26
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-01 17:37:34 +0300

    add AddressEntityUtil

---
+++ test-application/src/test/java/by/aurorasoft/testapp/util/AddressEntityUtil.java
@@ -0,0 +1,16 @@
+package by.aurorasoft.testapp.util;
+
+import by.aurorasoft.testapp.crud.entity.AddressEntity;
+import lombok.experimental.UtilityClass;
+
+import static org.junit.Assert.assertEquals;
+
+@UtilityClass
+public final class AddressEntityUtil {
+
+    public static void checkEquals(final AddressEntity expected, final AddressEntity actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getCountry(), expected.getCountry());
+        assertEquals(expected.getCity(), actual.getCity());
+    }
+}
