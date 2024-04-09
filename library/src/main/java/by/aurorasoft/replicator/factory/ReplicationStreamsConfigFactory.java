5ffbcc2e3f58c9dbdb7e509cf245cb6e5f20b6f6
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-09 17:38:07 +0300

    add ReplicationStreamsConfigFactory

---
+++ library/src/main/java/by/aurorasoft/replicator/factory/ReplicationStreamsConfigFactory.java
@@ -0,0 +1,28 @@
+package by.aurorasoft.replicator.factory;
+
+import by.aurorasoft.replicator.consuming.pipeline.ReplicationConsumePipeline;
+import org.apache.kafka.streams.StreamsConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static org.apache.kafka.streams.StreamsConfig.*;
+
+@Component
+public final class ReplicationStreamsConfigFactory {
+    private final String bootstrapAddress;
+
+    public ReplicationStreamsConfigFactory(@Value("${spring.kafka.bootstrap-servers}") final String bootstrapAddress) {
+        this.bootstrapAddress = bootstrapAddress;
+    }
+
+    public StreamsConfig create(final ReplicationConsumePipeline<?, ?> pipeline) {
+        final Map<String, Object> configsByNames = Map.of(
+                APPLICATION_ID_CONFIG, pipeline.getId(),
+                BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress,
+                PROCESSING_GUARANTEE_CONFIG, EXACTLY_ONCE_V2
+        );
+        return new StreamsConfig(configsByNames);
+    }
+}
---
+++ library/src/test/java/by/aurorasoft/replicator/factory/ReplicationKafkaStreamsFactoryTest.java
@@ -0,0 +1,11 @@
+package by.aurorasoft.replicator.factory;
+
+import org.junit.Test;
+
+public final class ReplicationKafkaStreamsFactoryTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
