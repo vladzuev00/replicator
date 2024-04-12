a67de8616ba0b21a6994b295032ceb6371788307
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-12 12:13:51 +0300

    refactor AbstractSpringBootTest

--- test-application/src/test/java/by/aurorasoft/testapp/base/AbstractSpringBootTest.java
+++ test-application/src/test/java/by/aurorasoft/testapp/base/AbstractSpringBootTest.java
@@ -1,13 +1,15 @@
 package by.aurorasoft.testapp.base;
 
-import by.aurorasoft.testapp.base.containerinitializer.DBContainerInitializer;
+import by.aurorasoft.testapp.base.containerinitializer.DataBaseContainerInitializer;
 import by.aurorasoft.testapp.base.containerinitializer.KafkaContainerInitializer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static java.time.ZoneOffset.UTC;
 import static java.util.TimeZone.getTimeZone;
@@ -15,8 +17,9 @@ import static java.util.TimeZone.setDefault;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
-@ContextConfiguration(initializers = {DBContainerInitializer.class, KafkaContainerInitializer.class})
+@ContextConfiguration(initializers = {DataBaseContainerInitializer.class, KafkaContainerInitializer.class})
 public abstract class AbstractSpringBootTest {
 
     @PersistenceContext
