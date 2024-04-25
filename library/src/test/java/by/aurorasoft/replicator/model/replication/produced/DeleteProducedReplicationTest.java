fbbd7e963b80eadebf9ea9233bf8dab06bab650c
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-25 10:57:28 +0300

    refactor DeleteProducedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/replication/produced/DeleteProducedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/replication/produced/DeleteProducedReplicationTest.java
@@ -2,10 +2,16 @@ package by.aurorasoft.replicator.model.replication.produced;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertSame;
+
 public final class DeleteProducedReplicationTest {
 
     @Test
     public void entityIdShouldBeGot() {
-//        final Object givenBody = new
+        final Object givenBody = new Object();
+        final DeleteProducedReplication givenReplication = new DeleteProducedReplication(null);
+
+        final Object actual = givenReplication.getEntityId(givenBody);
+        assertSame(givenBody, actual);
     }
 }
