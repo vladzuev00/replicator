f5a05824dca74fa4c6b89c4e86ff9ad8843aff18
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-22 12:39:30 +0300

    refactor ReplicationConfigTest

--- library/src/test/java/by/aurorasoft/replicator/config/ReplicationConfigTest.java
+++ library/src/test/java/by/aurorasoft/replicator/config/ReplicationConfigTest.java
@@ -29,37 +29,36 @@ public final class ReplicationConfigTest {
 
     @Test
     public void retryTemplateShouldBeCreated() {
-        final ReplicationRetryConsumeProperty givenProperty = new ReplicationRetryConsumeProperty(255, 10);
+        final long givenTimeLapseMs = 255;
+        final int givenMaxAttempts = 10;
+        final var givenProperty = new ReplicationRetryConsumeProperty(givenTimeLapseMs, givenMaxAttempts);
         final RetryTemplate givenTemplate = mock(RetryTemplate.class);
 
-        try (final MockedConstruction<RetryTemplateBuilder> mockedBuilderConstruction = mockConstruction(RetryTemplateBuilder.class, (builder, context) -> configureBuilder(builder, givenTemplate))) {
+        try (
+                final MockedConstruction<RetryTemplateBuilder> mockedBuilderConstruction = mockConstruction(
+                        RetryTemplateBuilder.class,
+                        (builder, context) -> configureBuilder(builder, givenTemplate)
+                )
+        ) {
             config.replicationRetryTemplate(givenProperty);
 
-            verifyConstruction(mockedBuilderConstruction);
-            final RetryTemplateBuilder builder = getConstructedObject(mockedBuilderConstruction);
+            assertEquals(1, mockedBuilderConstruction.constructed().size());
+            final RetryTemplateBuilder builder = mockedBuilderConstruction.constructed().get(0);
 
             final RetryTemplate actual = builder.build();
             assertSame(givenTemplate, actual);
 
-            verify(builder, times(1)).fixedBackoff(eq(givenProperty.getTimeLapseMs()));
-            verify(builder, times(1)).maxAttempts(eq(givenProperty.getMaxAttempts()));
+            verify(builder, times(1)).fixedBackoff(eq(givenTimeLapseMs));
+            verify(builder, times(1)).maxAttempts(eq(givenMaxAttempts));
             verify(builder, times(1)).retryOn(same(RelatedReplicationNotDeliveredException.class));
         }
     }
 
     @SuppressWarnings("unchecked")
-    private static void configureBuilder(final RetryTemplateBuilder builder, final RetryTemplate template) {
+    private void configureBuilder(final RetryTemplateBuilder builder, final RetryTemplate template) {
         when(builder.fixedBackoff(anyLong())).thenReturn(builder);
         when(builder.maxAttempts(anyInt())).thenReturn(builder);
         when(builder.retryOn(any(Class.class))).thenReturn(builder);
         when(builder.build()).thenReturn(template);
-    }
-
-    private void verifyConstruction(final MockedConstruction<?> construction) {
-        assertEquals(1, construction.constructed().size());
-    }
-
-    private <T> T getConstructedObject(final MockedConstruction<T> mockedConstruction) {
-        return mockedConstruction.constructed().get(0);
     }
 }
