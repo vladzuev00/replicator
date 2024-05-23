f9ca777d304f89b69e852ead46a498de493d5e09
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-23 17:16:50 +0300

    refactor ReplicationRetryConsumePropertyTest

--- library/src/test/java/by/aurorasoft/replicator/property/ReplicationRetryConsumePropertyTest.java
+++ library/src/test/java/by/aurorasoft/replicator/property/ReplicationRetryConsumePropertyTest.java
@@ -3,9 +3,16 @@ package by.aurorasoft.replicator.property;
 import by.aurorasoft.replicator.base.AbstractSpringBootTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.assertEquals;
 
+@SpringBootTest(
+        properties = {
+                "replication.consume.retry.time-lapse-ms=3000",
+                "replication.consume.retry.max-attempts=100"
+        }
+)
 public final class ReplicationRetryConsumePropertyTest extends AbstractSpringBootTest {
 
     @Autowired
@@ -13,7 +20,7 @@ public final class ReplicationRetryConsumePropertyTest extends AbstractSpringBoo
 
     @Test
     public void propertyShouldBeCreated() {
-        final ReplicationRetryConsumeProperty expected = new ReplicationRetryConsumeProperty(3000, 3);
+        final ReplicationRetryConsumeProperty expected = new ReplicationRetryConsumeProperty(3000, 100);
         assertEquals(expected, actual);
     }
 }
