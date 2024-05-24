4c5bf14eb1135301ebcdab9394f82f319bb0ff2c
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-24 16:44:04 +0300

    refactor PersonV1Service

--- test-application/src/main/java/by/aurorasoft/testapp/crud/v1/service/PersonV1Service.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/v1/service/PersonV1Service.java
@@ -1,14 +1,20 @@
 package by.aurorasoft.testapp.crud.v1.service;
 
+import by.aurorasoft.replicator.annotation.ReplicatedService;
+import by.aurorasoft.replicator.annotation.ReplicatedService.ProducerConfig;
+import by.aurorasoft.replicator.annotation.ReplicatedService.TopicConfig;
 import by.aurorasoft.testapp.crud.entity.PersonEntity;
 import by.aurorasoft.testapp.crud.repository.PersonRepository;
 import by.aurorasoft.testapp.crud.v1.dto.PersonV1;
 import by.aurorasoft.testapp.crud.v1.mapper.PersonV1Mapper;
 import by.nhorushko.crudgeneric.service.CrudGenericService;
-import org.springframework.stereotype.Service;
+import org.apache.kafka.common.serialization.LongSerializer;
 
-@Service
 @SuppressWarnings("deprecation")
+@ReplicatedService(
+        producerConfig = @ProducerConfig(idSerializer = LongSerializer.class),
+        topicConfig = @TopicConfig(name = "sync-person")
+)
 public class PersonV1Service extends CrudGenericService<PersonV1, PersonEntity, PersonRepository, PersonV1Mapper> {
 
     public PersonV1Service(final PersonRepository repository, final PersonV1Mapper mapper) {
