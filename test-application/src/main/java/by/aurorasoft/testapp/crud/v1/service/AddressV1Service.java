5c5d8dbb8a1daaa63100e494c1202b2858d55572
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-24 16:41:35 +0300

    refactor AddressV1Service

--- test-application/src/main/java/by/aurorasoft/testapp/crud/v1/service/AddressV1Service.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/v1/service/AddressV1Service.java
@@ -1,6 +1,8 @@
 package by.aurorasoft.testapp.crud.v1.service;
 
 import by.aurorasoft.replicator.annotation.ReplicatedService;
+import by.aurorasoft.replicator.annotation.ReplicatedService.ProducerConfig;
+import by.aurorasoft.replicator.annotation.ReplicatedService.TopicConfig;
 import by.aurorasoft.testapp.crud.entity.AddressEntity;
 import by.aurorasoft.testapp.crud.repository.AddressRepository;
 import by.aurorasoft.testapp.crud.v1.dto.AddressV1;
@@ -10,8 +12,8 @@ import org.apache.kafka.common.serialization.LongSerializer;
 
 @SuppressWarnings("deprecation")
 @ReplicatedService(
-        producerConfig = @ReplicatedService.ProducerConfig(idSerializer = LongSerializer.class),
-        topicConfig = @ReplicatedService.TopicConfig(name = "sync-address")
+        producerConfig = @ProducerConfig(idSerializer = LongSerializer.class),
+        topicConfig = @TopicConfig(name = "sync-address")
 )
 public class AddressV1Service extends CrudGenericService<AddressV1, AddressEntity, AddressRepository, AddressV1Mapper> {
 
