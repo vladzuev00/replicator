8b4c7621e840f5c755b687d58bef95873a4b24cb
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-24 16:47:04 +0300

    refactor PersonV2

--- test-application/src/main/java/by/aurorasoft/testapp/crud/v2/dto/PersonV2.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/v2/dto/PersonV2.java
@@ -2,7 +2,10 @@ package by.aurorasoft.testapp.crud.v2.dto;
 
 import by.aurorasoft.testapp.crud.dto.Person;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
-import lombok.*;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
@@ -13,7 +16,12 @@ public final class PersonV2 extends Person implements AbstractDto<Long> {
     private final AddressV2 address;
 
     @Builder
-    public PersonV2(Long id, String name, String surname, String patronymic, LocalDate birthDate, AddressV2 address) {
+    public PersonV2(final Long id,
+                    final String name,
+                    final String surname,
+                    final String patronymic,
+                    final LocalDate birthDate,
+                    final AddressV2 address) {
         super(id, name, surname, patronymic, birthDate);
         this.address = address;
     }
