46d84efdd1f73db6e5df2ab47a0ef529909c7196
Vlad Zuev <vladzuev.00@mail.ru>
2024-06-10 15:45:41 +0300

    refactor ReplicationIT, ReplicatedAddressEntityUtil and add EntityUtil

---
+++ test-application/src/test/java/by/aurorasoft/testapp/testutil/EntityUtil.java
@@ -0,0 +1,21 @@
+package by.aurorasoft.testapp.testutil;
+
+import by.aurorasoft.testapp.crud.entity.Entity;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import static java.util.stream.IntStream.range;
+import static org.junit.Assert.assertEquals;
+
+@UtilityClass
+public final class EntityUtil {
+
+    public static <E extends Entity> void checkEquals(final List<E> expected,
+                                                      final List<E> actual,
+                                                      final BiConsumer<E, E> equalChecker) {
+        assertEquals(expected.size(), actual.size());
+        range(0, expected.size()).forEach(i -> equalChecker.accept(expected.get(i), actual.get(i)));
+    }
+}
