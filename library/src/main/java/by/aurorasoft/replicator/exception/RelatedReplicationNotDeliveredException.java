cc69966e07dc8845447da3af679ad76a0ace8892
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-04 15:12:24 +0300

    refactor RelatedReplicationNotDeliveredException

--- library/src/main/java/by/aurorasoft/replicator/exception/RelatedReplicationNotDeliveredException.java
+++ library/src/main/java/by/aurorasoft/replicator/exception/RelatedReplicationNotDeliveredException.java
@@ -2,22 +2,7 @@ package by.aurorasoft.replicator.exception;
 
 public final class RelatedReplicationNotDeliveredException extends RuntimeException {
 
-    @SuppressWarnings("unused")
-    public RelatedReplicationNotDeliveredException() {
-
-    }
-
-    @SuppressWarnings("unused")
-    public RelatedReplicationNotDeliveredException(final String description) {
-        super(description);
-    }
-
-    public RelatedReplicationNotDeliveredException(final Exception cause) {
+    public RelatedReplicationNotDeliveredException(Exception cause) {
         super(cause);
-    }
-
-    @SuppressWarnings("unused")
-    public RelatedReplicationNotDeliveredException(final String description, final Exception cause) {
-        super(description, cause);
     }
 }
