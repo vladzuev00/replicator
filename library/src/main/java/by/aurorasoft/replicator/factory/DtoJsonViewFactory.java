bacecd5ade507e57eeb260dd17ff0593fb2da5a9
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-10 17:57:01 +0300

    refactor DtoJsonViewFactory

--- library/src/main/java/by/aurorasoft/replicator/factory/DtoJsonViewFactory.java
+++ library/src/main/java/by/aurorasoft/replicator/factory/DtoJsonViewFactory.java
@@ -2,14 +2,21 @@ package by.aurorasoft.replicator.factory;
 
 import by.aurorasoft.replicator.annotation.ReplicatedService.DtoViewConfig;
 import by.aurorasoft.replicator.model.view.DtoJsonView;
-import com.monitorjbl.json.Match;
 import org.springframework.stereotype.Component;
+
+import static com.monitorjbl.json.Match.match;
+import static java.util.Arrays.stream;
 
 @Component
 public final class DtoJsonViewFactory {
 
-    public <T> DtoJsonView<T> create(T dto, DtoViewConfig config) {
+    public <T> DtoJsonView<T> create(T dto, DtoViewConfig[] configs) {
         DtoJsonView<T> view = new DtoJsonView<>(dto);
-        view.onClass(Match.match().)
+        stream(configs).forEach(config -> putMatch(view, config));
+        return view;
+    }
+
+    private void putMatch(DtoJsonView<?> view, DtoViewConfig config) {
+        view.onClass(config.getClass(), match().include(config.includedFields()).exclude(config.excludedFields()));
     }
 }
