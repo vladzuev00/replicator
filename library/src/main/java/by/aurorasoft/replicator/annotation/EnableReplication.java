759ddd2c5e19c12257147a7963c8d8b937b17d31
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-12 17:12:57 +0300

    refactor

--- library/src/main/java/by/aurorasoft/replicator/annotation/EnableReplication.java
+++ library/src/main/java/by/aurorasoft/replicator/annotation/EnableReplication.java
@@ -3,14 +3,15 @@ package by.aurorasoft.replicator.annotation;
 import by.aurorasoft.replicator.config.ReplicationConfig;
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
 @SuppressWarnings("unused")
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
 @Import(ReplicationConfig.class)
 public @interface EnableReplication {
 
