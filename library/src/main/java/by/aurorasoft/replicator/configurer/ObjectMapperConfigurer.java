e14dcc1640c464f80de4f6c1ce88b69298987d61
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-15 11:28:59 +0300

    refactor ObjectMapperConfigurer

--- library/src/main/java/by/aurorasoft/replicator/configurer/ObjectMapperConfigurer.java
+++ library/src/main/java/by/aurorasoft/replicator/configurer/ObjectMapperConfigurer.java
@@ -2,15 +2,18 @@ package by.aurorasoft.replicator.configurer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.monitorjbl.json.JsonViewModule;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public final class ObjectMapperConfigurer {
+    private final ObjectMapper mapper;
 
     @EventListener(ContextRefreshedEvent.class)
-    public void configure(ObjectMapper mapper) {
+    public void configure() {
         mapper.registerModule(new JsonViewModule());
     }
 }
