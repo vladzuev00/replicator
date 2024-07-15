76fd65b2d88ff483396f07dc13d6ff67df6fbbe7
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-15 11:57:08 +0300

    refactor ReplicationConsumeStarterTest

--- library/src/test/java/by/aurorasoft/replicator/consuming/starter/ReplicationConsumeStarterTest.java
+++ library/src/test/java/by/aurorasoft/replicator/consuming/starter/ReplicationConsumeStarterTest.java
@@ -1,21 +1,21 @@
 package by.aurorasoft.replicator.consuming.starter;
 
 import by.aurorasoft.replicator.model.pipeline.ReplicationConsumePipeline;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public final class ReplicationConsumeStarterTest {
 
     @Mock
@@ -32,7 +32,7 @@ public final class ReplicationConsumeStarterTest {
     @Captor
     private ArgumentCaptor<ReplicationConsumePipeline<?, ?>> pipelineArgumentCaptor;
 
-    @Before
+    @BeforeEach
     public void initializeStarter() {
         starter = new ReplicationConsumeStarter(
                 List.of(firstMockedPipeline, secondMockedPipeline),
