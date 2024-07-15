a46096b30c11e40e62faf0fa43178015fde9c7a9
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-15 16:42:15 +0300

    refactor ReflectionUtil

--- library/src/test/java/by/aurorasoft/replicator/testutil/ReflectionUtil.java
+++ library/src/test/java/by/aurorasoft/replicator/testutil/ReflectionUtil.java
@@ -11,22 +11,22 @@ import static org.springframework.util.ReflectionUtils.findField;
 @UtilityClass
 public final class ReflectionUtil {
 
-    public static <V, T> V getFieldValue(final T target, final String fieldName, final Class<V> valueType) {
-        final Field field = getField(target, fieldName);
+    public static <V, T> V getFieldValue(T target, String fieldName, Class<V> valueType) {
+        Field field = getField(target, fieldName);
         field.setAccessible(true);
         try {
-            final Object value = getFieldValue(field, target);
+            Object value = getFieldValue(field, target);
             return valueType.cast(value);
         } finally {
             field.setAccessible(false);
         }
     }
 
-    private static Field getField(final Object target, final String fieldName) {
+    private static Field getField(Object target, String fieldName) {
         return requireNonNull(findField(target.getClass(), fieldName));
     }
 
-    private static Object getFieldValue(final Field field, final Object target) {
+    private static Object getFieldValue(Field field, Object target) {
         return ReflectionUtils.getField(field, target);
     }
 }
