69119ad74a94181826959bcc51d4442540d8b9f0
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-29 17:46:54 +0300

    add ReplicationConfig::replicationObjectMapper

--- library/src/main/java/by/aurorasoft/replicator/config/ReplicationConfig.java
+++ library/src/main/java/by/aurorasoft/replicator/config/ReplicationConfig.java
@@ -3,8 +3,11 @@ package by.aurorasoft.replicator.config;
 import by.aurorasoft.replicator.factory.ReplicatedServiceRegistryFactory;
 import by.aurorasoft.replicator.factory.ReplicationProducerRegistryFactory;
 import by.aurorasoft.replicator.factory.ReplicationRetryTemplateFactory;
+import by.aurorasoft.replicator.objectmapper.ReplicationObjectMapper;
+import by.aurorasoft.replicator.objectmapper.ReplicationObjectMapperFactory;
 import by.aurorasoft.replicator.registry.ReplicatedServiceRegistry;
 import by.aurorasoft.replicator.registry.ReplicationProducerRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -29,5 +32,11 @@ public class ReplicationConfig {
     @Bean
     public RetryTemplate replicationRetryTemplate(ReplicationRetryTemplateFactory factory) {
         return factory.create();
+    }
+
+    //TODO: test
+    @Bean
+    public ReplicationObjectMapper replicationObjectMapper(ReplicationObjectMapperFactory factory, ObjectMapper source) {
+        return factory.create(source);
     }
 }
