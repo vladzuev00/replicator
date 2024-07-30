552de7a36fd50e34ea99e8eb446c8fc555341536
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-30 12:38:13 +0300

    add viewConfigs for SecondTestV2CRUDService

--- library/src/test/java/by/aurorasoft/replicator/factory/ReplicationProducerRegistryFactoryTest.java
+++ library/src/test/java/by/aurorasoft/replicator/factory/ReplicationProducerRegistryFactoryTest.java
@@ -1,71 +1,84 @@
-//package by.aurorasoft.replicator.factory;
-//
-//import by.aurorasoft.replicator.producer.ReplicationProducer;
-//import by.aurorasoft.replicator.registry.ReplicationProducerRegistry;
-//import by.aurorasoft.replicator.registry.ReplicatedServiceRegistry;
-//import by.aurorasoft.replicator.v1.service.FirstTestV1CRUDService;
-//import by.aurorasoft.replicator.v2.service.SecondTestV2CRUDService;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//import java.util.LinkedHashSet;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Set;
-//
-//import static by.aurorasoft.replicator.testutil.ReflectionUtil.getFieldValue;
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.same;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public final class ReplicationProducerRegistryFactoryTest {
-//    private static final String FIELD_NAME_PRODUCERS_BY_SERVICES = "producersByServices";
-//
-//    @Mock
-//    private ReplicatedServiceRegistry mockedServiceRegistry;
-//
-//    @Mock
-//    private ReplicationProducerFactory mockedProducerFactory;
-//
-//    private ReplicationProducerRegistryFactory registryFactory;
-//
-//    @Before
-//    public void initializeRegistryFactory() {
-//        registryFactory = new ReplicationProducerRegistryFactory(mockedServiceRegistry, mockedProducerFactory);
-//    }
-//
-//    @Test
-//    public void registryShouldBeCreated() {
-//        Object firstGivenService = new FirstTestV1CRUDService();
-//        Object secondGivenService = new SecondTestV2CRUDService();
-//        Set<Object> givenServices = new LinkedHashSet<>(List.of(firstGivenService, secondGivenService));
-//        when(mockedServiceRegistry.getServices()).thenReturn(givenServices);
-//
-//        ReplicationProducer firstGivenProducer = mockProducerFor(firstGivenService);
-//        ReplicationProducer secondGivenProducer = mockProducerFor(secondGivenService);
-//
-//        ReplicationProducerRegistry actual = registryFactory.create();
-//        var actualProducersByServices = getProducersByServices(actual);
-//        var expectedProducersByServices = Map.of(
-//                firstGivenService, firstGivenProducer,
-//                secondGivenService, secondGivenProducer
-//        );
-//        assertEquals(expectedProducersByServices, actualProducersByServices);
-//    }
-//
-//    private ReplicationProducer mockProducerFor(Object service) {
-//        ReplicationProducer producer = mock(ReplicationProducer.class);
-//        when(mockedProducerFactory.create(same(service))).thenReturn(producer);
-//        return producer;
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    private Map<Object, ReplicationProducer> getProducersByServices(ReplicationProducerRegistry registry) {
-//        return getFieldValue(registry, FIELD_NAME_PRODUCERS_BY_SERVICES, Map.class);
-//    }
-//}
+package by.aurorasoft.replicator.factory;
+
+import by.aurorasoft.replicator.annotation.ReplicatedService;
+import by.aurorasoft.replicator.producer.ReplicationProducer;
+import by.aurorasoft.replicator.registry.ReplicatedServiceRegistry;
+import by.aurorasoft.replicator.registry.ReplicationProducerRegistry;
+import by.aurorasoft.replicator.v1.service.FirstTestV1CRUDService;
+import by.aurorasoft.replicator.v2.service.SecondTestV2CRUDService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static by.aurorasoft.replicator.testutil.ReflectionUtil.getFieldValue;
+import static java.util.stream.IntStream.range;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class ReplicationProducerRegistryFactoryTest {
+    private static final String FIELD_NAME_PRODUCERS_BY_SERVICES = "producersByServices";
+
+    @Mock
+    private ReplicatedServiceRegistry mockedServiceRegistry;
+
+    @Mock
+    private ReplicationProducerFactory mockedProducerFactory;
+
+    private ReplicationProducerRegistryFactory registryFactory;
+
+    @Captor
+    private ArgumentCaptor<ReplicatedService> replicatedServiceArgumentCaptor;
+
+    @Before
+    public void initializeRegistryFactory() {
+        registryFactory = new ReplicationProducerRegistryFactory(mockedServiceRegistry, mockedProducerFactory);
+    }
+
+    @Test
+    public void registryShouldBeCreated() {
+        Object firstGivenService = new FirstTestV1CRUDService();
+        Object secondGivenService = new SecondTestV2CRUDService();
+        Set<Object> givenServices = new LinkedHashSet<>(List.of(firstGivenService, secondGivenService));
+        when(mockedServiceRegistry.getServices()).thenReturn(givenServices);
+
+        ReplicationProducer firstGivenProducer = mock(ReplicationProducer.class);
+        ReplicationProducer secondGivenProducer = mock(ReplicationProducer.class);
+        when(mockedProducerFactory.create(any(ReplicatedService.class)))
+                .thenReturn(firstGivenProducer)
+                .thenReturn(secondGivenProducer);
+
+        ReplicationProducerRegistry actual = registryFactory.create();
+        var actualProducersByServices = getProducersByServices(actual);
+        var expectedProducersByServices = Map.of(
+                firstGivenService, firstGivenProducer,
+                secondGivenService, secondGivenProducer
+        );
+        assertEquals(expectedProducersByServices, actualProducersByServices);
+
+        //TODO: verifyReplicatedServices
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<Object, ReplicationProducer> getProducersByServices(ReplicationProducerRegistry registry) {
+        return getFieldValue(registry, FIELD_NAME_PRODUCERS_BY_SERVICES, Map.class);
+    }
+
+    private void verifyReplicatedServices(List<ReplicatedService> expected) {
+        verify(mockedProducerFactory, times(expected.size())).create(replicatedServiceArgumentCaptor.capture());
+        List<ReplicatedService> actual = replicatedServiceArgumentCaptor.getAllValues();
+        throw new RuntimeException();
+//        range(0, expected.size()).forEach(i -> );
+    }
+}
--- library/src/test/java/by/aurorasoft/replicator/v2/service/SecondTestV2CRUDService.java
+++ library/src/test/java/by/aurorasoft/replicator/v2/service/SecondTestV2CRUDService.java
@@ -3,6 +3,8 @@ package by.aurorasoft.replicator.v2.service;
 import by.aurorasoft.replicator.annotation.ReplicatedService;
 import by.aurorasoft.replicator.annotation.ReplicatedService.ProducerConfig;
 import by.aurorasoft.replicator.annotation.ReplicatedService.TopicConfig;
+import by.aurorasoft.replicator.annotation.ReplicatedService.ViewConfig;
+import by.aurorasoft.replicator.v2.dto.TestV2Dto;
 import org.apache.kafka.common.serialization.LongSerializer;
 
 @ReplicatedService(
@@ -16,7 +18,19 @@ import org.apache.kafka.common.serialization.LongSerializer;
                 name = "second-topic",
                 partitionCount = 2,
                 replicationFactor = 2
-        )
+        ),
+        viewConfigs = {
+                @ViewConfig(
+                        type = TestV2Dto.class,
+                        includedFields = "first-field",
+                        excludedFields = "second-field"
+                ),
+                @ViewConfig(
+                        type = TestV2Dto.class,
+                        includedFields = {"third-field", "fourth-field"},
+                        excludedFields = {"fifth-field", "sixth-field", "seventh-field"}
+                )
+        }
 )
 public class SecondTestV2CRUDService extends TestV2CRUDService {
 
