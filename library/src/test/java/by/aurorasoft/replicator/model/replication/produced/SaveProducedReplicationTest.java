2c07a6957bc0bcf4cdad5575e4b51b4a52cfd311
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-31 15:45:57 +0300

    refactor SaveProducedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/replication/produced/SaveProducedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/replication/produced/SaveProducedReplicationTest.java
@@ -1,21 +1,24 @@
-//package by.aurorasoft.replicator.model.replication.produced;
-//
-//import by.aurorasoft.replicator.model.view.DtoJsonView;
-//import by.aurorasoft.replicator.v2.dto.TestV2Dto;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertSame;
-//
-//
-//public final class SaveProducedReplicationTest {
-//
-//    @Test
-//    public void entityIdShouldBeGot() {
-//        Long givenId = 255L;
-//        DtoJsonView<?> givenDtoJsonView = new DtoJsonView<>(new TestV2Dto(givenId));
-//        SaveProducedReplication givenReplication = new SaveProducedReplication(null);
-//
-//        Object actual = givenReplication.getEntityId(givenDtoJsonView);
-//        assertSame(givenId, actual);
-//    }
-//}
+package by.aurorasoft.replicator.model.replication.produced;
+
+import by.aurorasoft.replicator.model.view.EntityJsonView;
+import by.aurorasoft.replicator.testentity.TestEntity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+
+public final class SaveProducedReplicationTest {
+
+    @Test
+    public void entityIdShouldBeGot() {
+        Long givenId = 255L;
+        TestEntity givenEntity = TestEntity.builder()
+                .id(givenId)
+                .build();
+        EntityJsonView<?> givenEntityJsonView = new EntityJsonView<>(givenEntity);
+        SaveProducedReplication givenReplication = new SaveProducedReplication(null);
+
+        Object actual = givenReplication.getEntityId(givenEntityJsonView);
+        assertSame(givenId, actual);
+    }
+}
