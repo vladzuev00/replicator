7341784a6d0bce1199c5ec4e83b0689c3d47f051
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-02 14:01:12 +0300

    refactor PersonRepository

--- test-application/src/main/java/by/aurorasoft/testapp/crud/repository/PersonRepository.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/repository/PersonRepository.java
@@ -1,9 +1,18 @@
 package by.aurorasoft.testapp.crud.repository;
 
+import by.aurorasoft.replicator.annotation.ReplicatedRepository;
+import by.aurorasoft.replicator.annotation.ReplicatedRepository.EntityView;
+import by.aurorasoft.replicator.annotation.ReplicatedRepository.Producer;
+import by.aurorasoft.replicator.annotation.ReplicatedRepository.Topic;
 import by.aurorasoft.testapp.crud.entity.PersonEntity;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PersonRepository extends JpaRepository<PersonEntity, Long>, JpaSpecificationExecutor<PersonEntity> {
+@ReplicatedRepository(
+        producer = @Producer(idSerializer = LongSerializer.class),
+        topic = @Topic(name = "person-sync"),
+        entityViews = @EntityView(type = PersonEntity.class, excludedFields = "patronymic")
+)
+public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
 
 }
