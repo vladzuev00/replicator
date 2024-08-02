7018c8344b83eaf2ab4f132561242aeafa69f925
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-02 15:43:37 +0300

    refactor

--- library/src/test/java/by/aurorasoft/replicator/factory/ReplicationObjectMapperWrapperFactoryTest.java
+++ library/src/test/java/by/aurorasoft/replicator/factory/ReplicationObjectMapperWrapperFactoryTest.java
@@ -4,21 +4,18 @@ import by.aurorasoft.replicator.mapperwrapper.ReplicationObjectMapperWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.web.config.SpringDataJacksonConfiguration.PageModule;
-import org.springframework.data.web.config.SpringDataWebSettings;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.DIRECT;
-
 
 public final class ReplicationObjectMapperWrapperFactoryTest {
     private final ReplicationObjectMapperWrapperFactory factory = new ReplicationObjectMapperWrapperFactory();
 
     @Test
     public void wrapperShouldBeCreated() {
-        ObjectMapper givenSource = new ObjectMapper().registerModule(new PageModule(new SpringDataWebSettings(DIRECT)));
+        ObjectMapper givenSource = new ObjectMapper().registerModule(new PageModule());
 
         ReplicationObjectMapperWrapper actual = factory.create(givenSource);
         Set<Object> actualModuleIds = actual.getMapper().getRegisteredModuleIds();
