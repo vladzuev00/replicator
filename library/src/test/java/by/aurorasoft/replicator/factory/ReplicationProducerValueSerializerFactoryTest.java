f6c253afda2406a3f6d46f71fd6b052c4a9d5cb9
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-02 12:29:22 +0300

    add ReplicationProducerValueSerializerFactoryTest

---
+++ library/src/test/java/by/aurorasoft/replicator/factory/ReplicationProducerValueSerializerFactoryTest.java
@@ -0,0 +1,49 @@
+package by.aurorasoft.replicator.factory;
+
+import by.aurorasoft.replicator.mapperwrapper.ReplicationObjectMapperWrapper;
+import by.aurorasoft.replicator.model.replication.produced.ProducedReplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Serializer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import static by.aurorasoft.replicator.testutil.ReflectionUtil.getFieldValue;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public final class ReplicationProducerValueSerializerFactoryTest {
+    private static final String FIELD_NAME_OBJECT_MAPPER = "objectMapper";
+
+    @Mock
+    private ReplicationObjectMapperWrapper mockedMapperWrapper;
+
+    private ReplicationProducerValueSerializerFactory factory;
+
+    @BeforeEach
+    public void initializeFactory() {
+        factory = new ReplicationProducerValueSerializerFactory(mockedMapperWrapper);
+    }
+
+    @Test
+    public void serializerShouldBeCreated() {
+        ObjectMapper givenObjectMapper = mock(ObjectMapper.class);
+        when(mockedMapperWrapper.getMapper()).thenReturn(givenObjectMapper);
+
+        Serializer<ProducedReplication<?>> actual = factory.create();
+        assertTrue(actual instanceof JsonSerializer);
+
+        ObjectMapper actualObjectMapper = getObjectMapper(((JsonSerializer<?>) actual));
+        assertSame(givenObjectMapper, actualObjectMapper);
+    }
+
+    private ObjectMapper getObjectMapper(JsonSerializer<?> serializer) {
+        return getFieldValue(serializer, FIELD_NAME_OBJECT_MAPPER, ObjectMapper.class);
+    }
+}
