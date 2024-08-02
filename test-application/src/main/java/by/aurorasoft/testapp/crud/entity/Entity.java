da3dbbe4428d2327e18e52c2afb9195f81cd2eac
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-02 13:57:57 +0300

    refactor Entity

--- test-application/src/main/java/by/aurorasoft/testapp/crud/entity/Entity.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/entity/Entity.java
@@ -6,8 +6,9 @@ import java.util.Objects;
 
 import static java.util.Objects.hash;
 
-@SuppressWarnings("deprecation")
-public abstract class Entity implements by.nhorushko.crudgeneric.v2.domain.AbstractEntity<Long>, by.nhorushko.crudgeneric.domain.AbstractEntity {
+public abstract class Entity {
+
+    public abstract Long getId();
 
     @Override
     @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
