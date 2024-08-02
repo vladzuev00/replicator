275729679b23cbc7dc31d0c9907fb04ef7723b41
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-02 13:59:48 +0300

    add @ReplicatedRepository for AddressRepository

--- test-application/src/main/java/by/aurorasoft/testapp/crud/repository/AddressRepository.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/repository/AddressRepository.java
@@ -1,9 +1,13 @@
 package by.aurorasoft.testapp.crud.repository;
 
+import by.aurorasoft.replicator.annotation.ReplicatedRepository;
+import by.aurorasoft.replicator.annotation.ReplicatedRepository.Producer;
+import by.aurorasoft.replicator.annotation.ReplicatedRepository.Topic;
 import by.aurorasoft.testapp.crud.entity.AddressEntity;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AddressRepository extends JpaRepository<AddressEntity, Long>, JpaSpecificationExecutor<AddressEntity> {
+@ReplicatedRepository(producer = @Producer(idSerializer = LongSerializer.class), topic = @Topic(name = "address-sync"))
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
 
 }
