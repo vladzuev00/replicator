1e4fb56267363561e884987a5b079083480e4873
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-05 17:12:57 +0300

    add ReplicationProducer

--- library/src/main/java/by/aurorasoft/replicator/model/component/ReplicationProducer.java
+++ library/src/main/java/by/aurorasoft/replicator/model/component/ReplicationProducer.java
@@ -7,15 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Getter
 public final class ReplicationProducer<E, ID> extends ReplicationComponent<E, ID> {
-    private final ProducerConfig producerConfig;
+    private final ProducerConfig config;
     private final EntityViewConfig[] entityViewConfigs;
 
     public ReplicationProducer(String topic,
                                JpaRepository<E, ID> repository,
-                               ProducerConfig producerConfig,
+                               ProducerConfig config,
                                EntityViewConfig[] entityViewConfigs) {
         super(topic, repository);
-        this.producerConfig = producerConfig;
+        this.config = config;
         this.entityViewConfigs = entityViewConfigs;
     }
 }
