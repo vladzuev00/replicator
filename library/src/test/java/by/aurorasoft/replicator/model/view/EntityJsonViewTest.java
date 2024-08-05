f2b711e3de3348d2488bf4b5fa0ce9c6c26aeb25
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-05 16:27:19 +0300

    refactor EntityJsonViewTest

--- library/src/test/java/by/aurorasoft/replicator/model/view/EntityJsonViewTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/view/EntityJsonViewTest.java
@@ -1,10 +1,33 @@
 package by.aurorasoft.replicator.model.view;
 
+import by.aurorasoft.replicator.base.AbstractSpringBootTest;
+import by.aurorasoft.replicator.mapperwrapper.ReplicationObjectMapperWrapper;
+import by.aurorasoft.replicator.testentity.TestEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
-public final class EntityJsonViewTest {
+public final class EntityJsonViewTest extends AbstractSpringBootTest {
+
+    @Autowired
+    private ReplicationObjectMapperWrapper mapperWrapper;
+
+    @Test
+    public void viewShouldBeSerializedToJson()
+            throws Exception {
+        EntityJsonView<?> givenView = new EntityJsonView<>(new TestEntity(255L, "first-value", "second-value"));
+
+        String actual = mapperWrapper.getMapper().writeValueAsString(givenView);
+        String expected = """
+                {
+                  "id": 255,
+                  "firstProperty": "first-value",
+                  "secondProperty": "second-value"
+                }""";
+        assertEquals(expected, actual, true);
+    }
 
     @Test
     public void entityShouldBeGot() {
