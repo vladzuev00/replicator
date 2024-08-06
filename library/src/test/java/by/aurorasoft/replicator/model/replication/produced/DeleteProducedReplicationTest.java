0ffe487183a543b7cb978d2b378f5917b8e8ad3a
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-06 14:10:16 +0300

    refactor DeleteProducedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/replication/produced/DeleteProducedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/replication/produced/DeleteProducedReplicationTest.java
@@ -1,18 +1,17 @@
-//package by.aurorasoft.replicator.model.replication.produced;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertSame;
-//
-//
-//public final class DeleteProducedReplicationTest {
-//
-//    @Test
-//    public void entityIdShouldBeGotInternally() {
-//        Object givenEntityId = new Object();
-//        DeleteProducedReplication givenReplication = new DeleteProducedReplication(null);
-//
-//        Object actual = givenReplication.getEntityIdInternal(givenEntityId);
-//        assertSame(givenEntityId, actual);
-//    }
-//}
+package by.aurorasoft.replicator.model.replication.produced;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public final class DeleteProducedReplicationTest {
+
+    @Test
+    public void entityIdShouldBeGotInternally() {
+        Object givenEntityId = new Object();
+        DeleteProducedReplication givenReplication = new DeleteProducedReplication(null);
+
+        Object actual = givenReplication.getEntityIdInternal(givenEntityId);
+        assertSame(givenEntityId, actual);
+    }
+}
