b36847050f6b16f093574f8585d5e92ab16a816c
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-06 14:06:13 +0300

    refactor DeleteConsumedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/replication/consumed/DeleteConsumedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/replication/consumed/DeleteConsumedReplicationTest.java
@@ -1,21 +1,20 @@
-//package by.aurorasoft.replicator.model.replication.consumed;
-//
-//import by.aurorasoft.replicator.testentity.TestEntity;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import static org.mockito.Mockito.*;
-//
-//public final class DeleteConsumedReplicationTest {
-//
-//    @Test
-//    public void replicationShouldBeExecutedInternally() {
-//        Long givenEntityId = 255L;
-//        var givenReplication = new DeleteConsumedReplication<TestEntity, Long>(givenEntityId);
-//        @SuppressWarnings("unchecked") JpaRepository<TestEntity, Long> givenRepository = mock(JpaRepository.class);
-//
-//        givenReplication.executeInternal(givenRepository);
-//
-//        verify(givenRepository, times(1)).deleteById(same(givenEntityId));
-//    }
-//}
+package by.aurorasoft.replicator.model.replication.consumed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import static org.mockito.Mockito.*;
+
+public final class DeleteConsumedReplicationTest {
+
+    @Test
+    public void replicationShouldBeExecutedInternally() {
+        Long givenEntityId = 255L;
+        DeleteConsumedReplication<Object, Long> givenReplication = new DeleteConsumedReplication<>(givenEntityId);
+        @SuppressWarnings("unchecked") JpaRepository<Object, Long> givenRepository = mock(JpaRepository.class);
+
+        givenReplication.executeInternal(givenRepository);
+
+        verify(givenRepository, times(1)).deleteById(same(givenEntityId));
+    }
+}
