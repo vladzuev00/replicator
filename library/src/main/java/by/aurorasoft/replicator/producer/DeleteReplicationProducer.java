553e11530d795b1856f4df51974e16c1b990d224
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-06 12:44:58 +0300

    refactor producers

--- library/src/main/java/by/aurorasoft/replicator/producer/DeleteReplicationProducer.java
+++ library/src/main/java/by/aurorasoft/replicator/producer/DeleteReplicationProducer.java
@@ -1,11 +1,23 @@
 package by.aurorasoft.replicator.producer;
 
 import by.aurorasoft.replicator.model.replication.produced.DeleteProducedReplication;
+import by.aurorasoft.replicator.model.replication.produced.ProducedReplication;
 import org.springframework.kafka.core.KafkaTemplate;
 
-public final class DeleteReplicationProducer extends ReplicationProducer<DeleteProducedReplication, Object> {
+public final class DeleteReplicationProducer extends ReplicationProducer<Object> {
 
-    public DeleteReplicationProducer(String topicName, KafkaTemplate<Object, DeleteProducedReplication> kafkaTemplate) {
+
+    public DeleteReplicationProducer(String topicName, KafkaTemplate<Object, ProducedReplication<?>> kafkaTemplate) {
         super(topicName, kafkaTemplate);
+    }
+
+    @Override
+    protected Object createReplicationBody(Object entityId) {
+        return entityId;
+    }
+
+    @Override
+    protected ProducedReplication<?> createReplication(Object entityId) {
+        return new DeleteProducedReplication(entityId);
     }
 }
--- library/src/main/java/by/aurorasoft/replicator/producer/SaveReplicationProducer.java
+++ library/src/main/java/by/aurorasoft/replicator/producer/SaveReplicationProducer.java
@@ -1,11 +1,22 @@
 package by.aurorasoft.replicator.producer;
 
-import by.aurorasoft.replicator.model.replication.produced.SaveProducedReplication;
+import by.aurorasoft.replicator.model.replication.produced.ProducedReplication;
+import by.aurorasoft.replicator.model.view.EntityJsonView;
 import org.springframework.kafka.core.KafkaTemplate;
 
-public final class SaveReplicationProducer extends ReplicationProducer<SaveProducedReplication, Object> {
+public final class SaveReplicationProducer extends ReplicationProducer<EntityJsonView<?>> {
 
-    public SaveReplicationProducer(String topicName, KafkaTemplate<Object, SaveProducedReplication> kafkaTemplate) {
+    public SaveReplicationProducer(String topicName, KafkaTemplate<Object, ProducedReplication<?>> kafkaTemplate) {
         super(topicName, kafkaTemplate);
+    }
+
+    @Override
+    protected EntityJsonView<?> createReplicationBody(Object entity) {
+        return null;
+    }
+
+    @Override
+    protected ProducedReplication<?> createReplication(EntityJsonView<?> entityJsonView) {
+        return null;
     }
 }
