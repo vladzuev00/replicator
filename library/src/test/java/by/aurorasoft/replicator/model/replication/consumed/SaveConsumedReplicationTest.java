da4939d3dc86e055c9bbd37f782bec9a98385dfa
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-06 14:07:55 +0300

    refactor SaveConsumedReplicationTest

--- library/src/test/java/by/aurorasoft/replicator/model/replication/consumed/SaveConsumedReplicationTest.java
+++ library/src/test/java/by/aurorasoft/replicator/model/replication/consumed/SaveConsumedReplicationTest.java
@@ -1,21 +1,20 @@
-//package by.aurorasoft.replicator.model.replication.consumed;
-//
-//import by.aurorasoft.replicator.testentity.TestEntity;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import static org.mockito.Mockito.*;
-//
-//public final class SaveConsumedReplicationTest {
-//
-//    @Test
-//    public void replicationShouldBeExecutedInternally() {
-//        TestEntity givenEntity = TestEntity.builder().build();
-//        SaveConsumedReplication<TestEntity, Long> givenReplication = new SaveConsumedReplication<>(givenEntity);
-//        @SuppressWarnings("unchecked") JpaRepository<TestEntity, Long> givenRepository = mock(JpaRepository.class);
-//
-//        givenReplication.executeInternal(givenRepository);
-//
-//        verify(givenRepository, times(1)).save(same(givenEntity));
-//    }
-//}
+package by.aurorasoft.replicator.model.replication.consumed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import static org.mockito.Mockito.*;
+
+public final class SaveConsumedReplicationTest {
+
+    @Test
+    public void replicationShouldBeExecutedInternally() {
+        Object givenEntity = new Object();
+        SaveConsumedReplication<Object, Long> givenReplication = new SaveConsumedReplication<>(givenEntity);
+        @SuppressWarnings("unchecked") JpaRepository<Object, Long> givenRepository = mock(JpaRepository.class);
+
+        givenReplication.executeInternal(givenRepository);
+
+        verify(givenRepository, times(1)).save(same(givenEntity));
+    }
+}
