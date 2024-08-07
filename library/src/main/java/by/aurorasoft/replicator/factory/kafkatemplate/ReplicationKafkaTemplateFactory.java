7941e0d41c4b29970e8f9dd1e44d3aef342cea19
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-07 15:04:09 +0300

    refactor ReplicationKafkaTemplateFactory

--- library/src/main/java/by/aurorasoft/replicator/factory/kafkatemplate/ReplicationKafkaTemplateFactory.java
+++ library/src/main/java/by/aurorasoft/replicator/factory/kafkatemplate/ReplicationKafkaTemplateFactory.java
@@ -2,18 +2,25 @@ package by.aurorasoft.replicator.factory.kafkatemplate;
 
 import by.aurorasoft.replicator.model.replication.produced.ProducedReplication;
 import by.aurorasoft.replicator.model.setting.ReplicationProducerSetting;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.serialization.Serializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
+@RequiredArgsConstructor
 public final class ReplicationKafkaTemplateFactory {
+    private final ReplicationKafkaTemplateConfigsFactory configsFactory;
+    private final ReplicationKafkaTemplateKeySerializerFactory keySerializerFactory;
+    private final ReplicationKafkaTemplateValueSerializerFactory valueSerializerFactory;
 
     public KafkaTemplate<Object, ProducedReplication<?>> create(ReplicationProducerSetting<?, ?> setting) {
-        Map<String, Object> configsByKeys = configsFactory.create(config);
-//        Serializer<Object> keySerializer = keySerializerFactory.create(config);
-//        Serializer<ProducedReplication<?>> valueSerializer = valueSerializerFactory.create();
-//        var producerFactory = new DefaultKafkaProducerFactory<>(configsByKeys, keySerializer, valueSerializer);
-//        KafkaTemplate<Object, ProducedReplication<?>> kafkaTemplate = new KafkaTemplate<>(producerFactory);
-//        return new ReplicationProducer(topicName, kafkaTemplate);
+        Map<String, Object> configsByKeys = configsFactory.create(setting);
+        Serializer<Object> keySerializer = keySerializerFactory.create(setting);
+        Serializer<ProducedReplication<?>> valueSerializer = valueSerializerFactory.create();
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configsByKeys, keySerializer, valueSerializer));
     }
 }
