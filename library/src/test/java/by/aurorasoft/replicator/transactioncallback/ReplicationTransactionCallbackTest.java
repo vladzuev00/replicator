5710085a1af1384be498c212555700c662fec9e1
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-08 17:14:27 +0300

    add ReplicationTransactionCallbackTest

---
+++ library/src/test/java/by/aurorasoft/replicator/transactioncallback/ReplicationTransactionCallbackTest.java
@@ -0,0 +1,32 @@
+package by.aurorasoft.replicator.transactioncallback;
+
+import by.aurorasoft.replicator.producer.ReplicationProducer;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public final class ReplicationTransactionCallbackTest {
+
+    @Test
+    public void bodyShouldBeProduced() {
+        Object givenBody = new Object();
+        ReplicationProducer givenProducer = mock(ReplicationProducer.class);
+        ReplicationTransactionCallback givenCallback = new TestReplicationTransactionCallback(givenBody, givenProducer);
+
+        givenCallback.afterCommit();
+
+        verify(givenProducer, times(1)).produceSave(same(givenBody));
+    }
+
+    private static final class TestReplicationTransactionCallback extends ReplicationTransactionCallback {
+
+        public TestReplicationTransactionCallback(Object body, ReplicationProducer producer) {
+            super(body, producer);
+        }
+
+        @Override
+        protected void produce(Object body, ReplicationProducer producer) {
+            producer.produceSave(body);
+        }
+    }
+}
