e2bcb17d82de51cae967a961b5880e3ec5783aa4
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-09 12:06:30 +0300

    refactor AddressRepository and PersonRepository

--- test-application/src/main/java/by/aurorasoft/testapp/crud/repository/AddressRepository.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/repository/AddressRepository.java
@@ -1,13 +1,8 @@
 package by.aurorasoft.testapp.crud.repository;
 
-import by.aurorasoft.replicator.annotation.ReplicatedRepository;
-import by.aurorasoft.replicator.annotation.ReplicatedRepository.Producer;
-import by.aurorasoft.replicator.annotation.ReplicatedRepository.Topic;
 import by.aurorasoft.testapp.crud.entity.AddressEntity;
-import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@ReplicatedRepository(producer = @Producer(idSerializer = LongSerializer.class), topic = @Topic(name = "address-sync"))
 public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
 
 }
--- test-application/src/main/java/by/aurorasoft/testapp/crud/repository/PersonRepository.java
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/repository/PersonRepository.java
@@ -1,18 +1,8 @@
 package by.aurorasoft.testapp.crud.repository;
 
-import by.aurorasoft.replicator.annotation.ReplicatedRepository;
-import by.aurorasoft.replicator.annotation.ReplicatedRepository.EntityView;
-import by.aurorasoft.replicator.annotation.ReplicatedRepository.Producer;
-import by.aurorasoft.replicator.annotation.ReplicatedRepository.Topic;
 import by.aurorasoft.testapp.crud.entity.PersonEntity;
-import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@ReplicatedRepository(
-        producer = @Producer(idSerializer = LongSerializer.class),
-        topic = @Topic(name = "person-sync"),
-        entityViews = @EntityView(type = PersonEntity.class, excludedFields = "patronymic")
-)
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
 
 }
