8c46f7acbccb0ba9a08d1f85868a0588427b0009
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-09 17:01:32 +0300

    refactor ReplicationProduceConfig

--- test-application/src/main/java/by/aurorasoft/testapp/config/ReplicationProduceConfig.java
+++ test-application/src/main/java/by/aurorasoft/testapp/config/ReplicationProduceConfig.java
@@ -1,8 +1,11 @@
 package by.aurorasoft.testapp.config;
 
 import by.aurorasoft.replicator.model.setting.ReplicationProduceSetting;
+import by.aurorasoft.testapp.crud.entity.AddressEntity;
 import by.aurorasoft.testapp.crud.entity.PersonEntity;
+import by.aurorasoft.testapp.crud.repository.AddressRepository;
 import by.aurorasoft.testapp.crud.repository.PersonRepository;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +13,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ReplicationProduceConfig {
 
-//    @Bean
-//    public ReplicationProduceSetting<PersonEntity, Long> personReplicationProduceSetting(@Value("${replication.consume.topic.person}") String topic,
-//                                                                                         PersonRepository repository) {
-//        return new ReplicationProduceSetting<>(topic, repository, )
-//    }
+    @Bean
+    public LongSerializer longSerializer() {
+        return new LongSerializer();
+    }
+
+    @Bean
+    public ReplicationProduceSetting<PersonEntity, Long> personReplicationProduceSetting(@Value("${replication.consume.topic.person}") String topic,
+                                                                                         PersonRepository repository) {
+        return ReplicationProduceSetting.<PersonEntity, Long>builder()
+                .topic(topic)
+                .repository(repository)
+                .idSerializer(longSerializer())
+                .build();
+    }
+
+    @Bean
+    public ReplicationProduceSetting<AddressEntity, Long> addressReplicationProduceSetting(@Value("${replication.consume.topic.address}") String topic,
+                                                                                           AddressRepository repository) {
+        return ReplicationProduceSetting.<AddressEntity, Long>builder()
+                .topic(topic)
+                .repository(repository)
+                .idSerializer(longSerializer())
+                .build();
+    }
 }
