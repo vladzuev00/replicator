8a4f2f8d2dd62f825663a77b97370c7814eb498c
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-09 14:40:20 +0300

    refactor ReplicationUniqueTopicCheckerTest

--- library/src/test/java/by/aurorasoft/replicator/validator/ReplicationUniqueTopicCheckerTest.java
+++ library/src/test/java/by/aurorasoft/replicator/validator/ReplicationUniqueTopicCheckerTest.java
@@ -1,7 +1,10 @@
 package by.aurorasoft.replicator.validator;
 
 import by.aurorasoft.replicator.model.setting.ReplicationSetting;
+import by.aurorasoft.replicator.testentity.TestEntity;
+import by.aurorasoft.replicator.testrepository.TestRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 
@@ -17,10 +20,11 @@ public final class ReplicationUniqueTopicCheckerTest {
         assertSame(givenTopic, actual);
     }
 
-    private static final class TestReplicationSetting extends ReplicationSetting<Object, Object> {
+    private static final class TestReplicationSetting extends ReplicationSetting<TestEntity, Long> {
+        private static final JpaRepository<TestEntity, Long> GIVEN_REPOSITORY = new TestRepository();
 
         public TestReplicationSetting(String topic) {
-            super(topic, null);
+            super(topic, GIVEN_REPOSITORY);
         }
     }
 }
