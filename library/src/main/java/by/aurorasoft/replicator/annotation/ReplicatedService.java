c7b33f825a60c8bb8109b1f4ca8866005320ba17
Vlad Zuev <vladzuev.00@mail.ru>
2024-08-14 10:32:33 +0300

    add ReplicatedService

---
+++ library/src/main/java/by/aurorasoft/replicator/annotation/ReplicatedService.java
@@ -0,0 +1,47 @@
+package by.aurorasoft.replicator.annotation;
+
+import org.springframework.core.serializer.Serializer;
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Service
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface ReplicatedService {
+    ProducerConfig producerConfig();
+
+    TopicConfig topicConfig();
+
+    ViewConfig[] viewConfigs() default {};
+
+    @interface ProducerConfig {
+        Class<? extends Serializer<?>> idSerializer();
+
+        int batchSize() default 10;
+
+        int lingerMs() default 500;
+
+        int deliveryTimeoutMs() default 100000;
+    }
+
+    @interface TopicConfig {
+        String name();
+
+        int partitionCount() default 1;
+
+        short replicationFactor() default 1;
+    }
+
+    @interface ViewConfig {
+        Class<?> type();
+
+        String[] includedFields() default {};
+
+        String[] excludedFields() default {};
+    }
+}
