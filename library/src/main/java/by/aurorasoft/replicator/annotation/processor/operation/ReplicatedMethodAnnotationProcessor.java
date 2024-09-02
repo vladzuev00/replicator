18781b8be002ad17f1d64f6077d00406f230d9ef
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-02 11:34:23 +0300

    refactor ReplicatedMethodAnnotationProcessor

--- library/src/main/java/by/aurorasoft/replicator/annotation/processor/operation/ReplicatedMethodAnnotationProcessor.java
+++ library/src/main/java/by/aurorasoft/replicator/annotation/processor/operation/ReplicatedMethodAnnotationProcessor.java
@@ -1,60 +1,44 @@
 package by.aurorasoft.replicator.annotation.processor.operation;
 
+import by.aurorasoft.replicator.annotation.ReplicatedService;
 import by.aurorasoft.replicator.annotation.processor.ReplicaAnnotationProcessor;
 
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
 import java.lang.annotation.Annotation;
-import java.util.Set;
+import java.util.List;
 import java.util.stream.Stream;
 
 public abstract class ReplicatedMethodAnnotationProcessor extends ReplicaAnnotationProcessor<ExecutableElement> {
-    private static final String TEMPLATE_CLASS_REQUIREMENT = "It should be inside class annotated by @%s";
+    private static final String CLASS_REQUIREMENT = "It should be inside class annotated by @"
+            + ReplicatedService.class.getSimpleName();
 
     public ReplicatedMethodAnnotationProcessor(Class<? extends Annotation> annotation) {
         super(annotation, ExecutableElement.class);
     }
 
     @Override
-    protected boolean isValidPublicElement(ExecutableElement element) {
-        return false;
+    protected final boolean isValidPublicElement(ExecutableElement element) {
+        return isInsideReplicatedService(element)
+                && isValidReturnType(element.getReturnType())
+                && isValidParameters(element.getParameters());
     }
 
     @Override
-    protected Stream<String> getRequirementsInternal() {
-        return null;
+    protected final Stream<String> getRequirementsInternal() {
+        return Stream.of(CLASS_REQUIREMENT, getReturnTypeRequirement(), getParametersRequirement());
     }
 
+    protected abstract boolean isValidReturnType(TypeMirror type);
 
-//    public ReplicatedOperationProcessor(Class<? extends Annotation> annotation) {
-//        super(annotation, ExecutableElement.class);
-//    }
+    protected abstract boolean isValidParameters(List<? extends VariableElement> parameters);
 
-//    @Override
-//    protected final boolean isValid(ExecutableElement method) {
-//        return isPublic(method) && isInsideReplicatedService(method) && isValidParameters(method.getParameters());
-//    }
-//
-//    @Override
-//    protected final Set<String> getRequirements() {
-//        return concat(
-//                Stream.of(getClassRequirement(), MODIFIER_REQUIREMENT),
-//                getRequirementsInternal()
-//        ).collect(toUnmodifiableSet());
-//    }
-//
-//    protected abstract boolean isValidParameters(List<? extends VariableElement> parameters);
-//
-//    protected abstract Stream<String> getRequirementsInternal();
-//
-//    private boolean isPublic(ExecutableElement method) {
-//        return method.getModifiers().contains(PUBLIC);
-//    }
-//
-//    private boolean isInsideReplicatedService(ExecutableElement method) {
-//        return method.getEnclosingElement().getAnnotation(ReplicatedService.class) != null;
-//    }
-//
-//    private String getClassRequirement() {
-//        return TEMPLATE_CLASS_REQUIREMENT.formatted(ReplicatedService.class.getSimpleName());
-//    }
+    protected abstract String getReturnTypeRequirement();
+
+    protected abstract String getParametersRequirement();
+
+    private boolean isInsideReplicatedService(ExecutableElement method) {
+        return method.getEnclosingElement().getAnnotation(ReplicatedService.class) != null;
+    }
 }
