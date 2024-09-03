098dad010b605369549e42eab8b35f59b4259a70
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-03 11:47:35 +0300

    add ReplicatedDeleteByIdProcessor

---
+++ library/src/main/java/by/aurorasoft/replicator/annotation/processor/operation/ReplicatedDeleteByIdProcessor.java
@@ -0,0 +1,51 @@
+package by.aurorasoft.replicator.annotation.processor.operation;
+
+import by.aurorasoft.replicator.annotation.operation.ReplicatedDeleteById;
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.Processor;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+
+@AutoService(Processor.class)
+public final class ReplicatedDeleteByIdProcessor extends ReplicatedMethodAnnotationProcessor {
+    private static final String PARAMETERS_REQUIREMENT = "Method should have at least one parameter as id";
+
+    public ReplicatedDeleteByIdProcessor() {
+        super(ReplicatedDeleteById.class);
+    }
+
+    @Override
+    protected boolean isValidReplicatedService(TypeMirror mirror) {
+        return true;
+    }
+
+    @Override
+    protected boolean isValidReturnType(TypeMirror mirror) {
+        return true;
+    }
+
+    @Override
+    protected boolean isValidParameters(List<? extends VariableElement> elements) {
+        return elements.size() > 1;
+    }
+
+    @Override
+    protected Optional<String> getReplicatedServiceRequirement() {
+        return empty();
+    }
+
+    @Override
+    protected Optional<String> getReturnTypeRequirement() {
+        return empty();
+    }
+
+    @Override
+    protected Optional<String> getParametersRequirement() {
+        return Optional.of(PARAMETERS_REQUIREMENT);
+    }
+}
