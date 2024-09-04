06b3dea8a0a55481ce6b9a469fc295d4fdeda7c8
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-04 18:05:02 +0300

    refactor

--- library/src/main/java/by/aurorasoft/replicator/model/setting/ReplicationSetting.java
+++ library/src/main/java/by/aurorasoft/replicator/model/setting/ReplicationSetting.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import static java.util.Objects.requireNonNull;
 
+//TODO: remove
 @Getter
 public abstract class ReplicationSetting<E, ID> {
     private final String topic;
