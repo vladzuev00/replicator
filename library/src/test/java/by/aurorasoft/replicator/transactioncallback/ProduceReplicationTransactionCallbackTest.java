f75eae5c79902e59f6a726a182b692b0fd313eb0
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-05 12:54:24 +0300

    refactor ProduceReplicationTransactionCallbackTest

--- library/src/test/java/by/aurorasoft/replicator/transactioncallback/ProduceReplicationTransactionCallbackTest.java
+++ library/src/test/java/by/aurorasoft/replicator/transactioncallback/ProduceReplicationTransactionCallbackTest.java
@@ -1,50 +1,49 @@
-//package by.aurorasoft.replicator.transactioncallback;
-//
-//import by.aurorasoft.replicator.model.replication.produced.ProducedReplication;
-//import by.aurorasoft.replicator.transactioncallback.ProduceReplicationTransactionCallback;
-//import org.apache.kafka.clients.producer.ProducerRecord;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.Captor;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.kafka.core.KafkaTemplate;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//public final class ProduceReplicationTransactionCallbackTest {
-//    private static final String GIVEN_TOPIC = "test-topic";
-//
-//    @Mock
-//    private KafkaTemplate<Object, ProducedReplication<?>> mockedKafkaTemplate;
-//
-//    @Mock
-//    private ProducedReplication<?> mockedReplication;
-//
-//    private ProduceReplicationTransactionCallback callback;
-//
-//    @Captor
-//    private ArgumentCaptor<ProducerRecord<Object, ProducedReplication<?>>> recordCaptor;
-//
-//    @BeforeEach
-//    public void initializeCallback() {
-//        callback = new ProduceReplicationTransactionCallback(mockedKafkaTemplate, mockedReplication, GIVEN_TOPIC);
-//    }
-//
-//    @Test
-//    public void callbackShouldBeCalled() {
-//        Object givenEntityId = new Object();
-//        when(mockedReplication.getDtoId()).thenReturn(givenEntityId);
-//
-//        callback.afterCommit();
-//
-//        verify(mockedKafkaTemplate, times(1)).send(recordCaptor.capture());
-//        ProducerRecord<Object, ProducedReplication<?>> actual = recordCaptor.getValue();
-//        var expected = new ProducerRecord<>(GIVEN_TOPIC, givenEntityId, mockedReplication);
-//        assertEquals(expected, actual);
-//    }
-//}
+package by.aurorasoft.replicator.transactioncallback;
+
+import by.aurorasoft.replicator.model.replication.produced.ProducedReplication;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public final class ProduceReplicationTransactionCallbackTest {
+    private static final String GIVEN_TOPIC = "test-topic";
+
+    @Mock
+    private ProducedReplication<?> mockedReplication;
+
+    @Mock
+    private KafkaTemplate<Object, ProducedReplication<?>> mockedKafkaTemplate;
+
+    private ProduceReplicationTransactionCallback callback;
+
+    @Captor
+    private ArgumentCaptor<ProducerRecord<Object, ProducedReplication<?>>> recordCaptor;
+
+    @BeforeEach
+    public void initializeCallback() {
+        callback = new ProduceReplicationTransactionCallback(GIVEN_TOPIC, mockedReplication, mockedKafkaTemplate);
+    }
+
+    @Test
+    public void callbackShouldBeCalled() {
+        Object givenDtoId = new Object();
+        when(mockedReplication.getDtoId()).thenReturn(givenDtoId);
+
+        callback.afterCommit();
+
+        verify(mockedKafkaTemplate, times(1)).send(recordCaptor.capture());
+        ProducerRecord<Object, ProducedReplication<?>> actual = recordCaptor.getValue();
+        var expected = new ProducerRecord<>(GIVEN_TOPIC, givenDtoId, mockedReplication);
+        assertEquals(expected, actual);
+    }
+}
