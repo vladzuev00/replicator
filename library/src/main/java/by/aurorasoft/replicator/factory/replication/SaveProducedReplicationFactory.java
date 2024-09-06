dd458bd10e68cc5aaeff93fd06ce962b5f1dc598
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-06 14:58:52 +0300

    refactor SaveProducedReplicationFactory and SaveProducedReplicationFactoryTest

--- library/src/main/java/by/aurorasoft/replicator/factory/replication/SaveProducedReplicationFactory.java
+++ library/src/main/java/by/aurorasoft/replicator/factory/replication/SaveProducedReplicationFactory.java
@@ -1,26 +1,27 @@
 package by.aurorasoft.replicator.factory.replication;
 
-import by.aurorasoft.replicator.annotation.service.ReplicatedService;
+import by.aurorasoft.replicator.annotation.service.ReplicatedService.DtoViewConfig;
 import by.aurorasoft.replicator.model.replication.produced.SaveProducedReplication;
+import by.aurorasoft.replicator.model.view.DtoJsonView;
 import org.springframework.stereotype.Component;
 
+import static com.monitorjbl.json.Match.match;
 import static java.util.Arrays.stream;
 
 @Component
 public final class SaveProducedReplicationFactory {
 
-    public SaveProducedReplication create(Object savedEntity, ReplicatedService.DtoViewConfig[] dtoViewConfigs) {
-        return null;
-//        EntityJsonView<?> view = new EntityJsonView<>(savedEntity);
-//        applySettings(view, viewSettings);
-//        return new SaveProducedReplication(view);
+    public SaveProducedReplication create(Object saveDto, DtoViewConfig[] dtoViewConfigs) {
+        DtoJsonView<?> dtoJsonView = new DtoJsonView<>(saveDto);
+        applyConfigs(dtoJsonView, dtoViewConfigs);
+        return new SaveProducedReplication(dtoJsonView);
     }
 
-//    private void applySettings(EntityJsonView<?> view, EntityViewSetting[] settings) {
-//        stream(settings).forEach(setting -> applySetting(view, setting));
-//    }
-//
-//    private void applySetting(EntityJsonView<?> view, EntityViewSetting setting) {
-//        view.onClass(setting.getType(), match().exclude(setting.getExcludedFields()));
-//    }
+    private void applyConfigs(DtoJsonView<?> view, DtoViewConfig[] configs) {
+        stream(configs).forEach(config -> applyConfig(view, config));
+    }
+
+    private void applyConfig(DtoJsonView<?> view, DtoViewConfig config) {
+        view.onClass(config.type(), match().include(config.includedFields()).exclude(config.excludedFields()));
+    }
 }
--- library/src/test/java/by/aurorasoft/replicator/factory/replication/SaveProducedReplicationFactoryTest.java
+++ library/src/test/java/by/aurorasoft/replicator/factory/replication/SaveProducedReplicationFactoryTest.java
@@ -1,32 +1,35 @@
-//package by.aurorasoft.replicator.factory.replication;
-//
-//import by.aurorasoft.replicator.model.replication.produced.SaveProducedReplication;
-//import by.aurorasoft.replicator.model.view.EntityJsonView;
-//import by.aurorasoft.replicator.testcrud.TestEntity;
-//import org.junit.jupiter.api.Test;
-//
-//import static com.monitorjbl.json.Match.match;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertSame;
-//
-//public final class SaveProducedReplicationFactoryTest {
-//    private final SaveProducedReplicationFactory factory = new SaveProducedReplicationFactory();
-//
-//    @Test
-//    public void replicationShouldBeCreated() {
-//        TestEntity givenEntity = new TestEntity();
-//        String givenExcludedField = "secondProperty";
-//        EntityViewSetting[] givenViewSettings = {
-//                new EntityViewSetting(TestEntity.class, new String[]{givenExcludedField})
-//        };
-//
-//        SaveProducedReplication actual = factory.create(givenEntity, givenViewSettings);
-//        EntityJsonView<TestEntity> expectedView = new EntityJsonView<>(givenEntity);
-//        expectedView.onClass(TestEntity.class, match().exclude(givenExcludedField));
-//        SaveProducedReplication expected = new SaveProducedReplication(expectedView);
-//        assertEquals(expected, actual);
-//
-//        Object actualEntity = actual.getBody().getEntity();
-//        assertSame(givenEntity, actualEntity);
-//    }
-//}
+package by.aurorasoft.replicator.factory.replication;
+
+import by.aurorasoft.replicator.annotation.service.ReplicatedService.DtoViewConfig;
+import by.aurorasoft.replicator.model.replication.produced.SaveProducedReplication;
+import by.aurorasoft.replicator.model.view.DtoJsonView;
+import by.aurorasoft.replicator.testcrud.TestDto;
+import org.junit.jupiter.api.Test;
+
+import static by.aurorasoft.replicator.testutil.DtoViewConfigUtil.createDtoViewConfig;
+import static com.monitorjbl.json.Match.match;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public final class SaveProducedReplicationFactoryTest {
+    private final SaveProducedReplicationFactory factory = new SaveProducedReplicationFactory();
+
+    @Test
+    public void replicationShouldBeCreated() {
+        TestDto givenDto = TestDto.builder().build();
+        String givenIncludedField = "firstProperty";
+        String givenExcludedField = "secondProperty";
+        DtoViewConfig[] givenDtoViewConfigs = {
+                createDtoViewConfig(TestDto.class, new String[]{givenIncludedField}, new String[]{givenExcludedField})
+        };
+
+        SaveProducedReplication actual = factory.create(givenDto, givenDtoViewConfigs);
+        DtoJsonView<TestDto> expectedView = new DtoJsonView<>(givenDto);
+        expectedView.onClass(TestDto.class, match().include(givenIncludedField).exclude(givenExcludedField));
+        SaveProducedReplication expected = new SaveProducedReplication(expectedView);
+        assertEquals(expected, actual);
+
+        Object actualDto = actual.getBody().getDto();
+        assertSame(givenDto, actualDto);
+    }
+}
