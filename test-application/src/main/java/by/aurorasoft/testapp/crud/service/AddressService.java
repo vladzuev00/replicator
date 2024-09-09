73d70e2270dabc85f96be6f0f6361f3a9e563cfc
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-09 16:55:48 +0300

    add AddressService

---
+++ test-application/src/main/java/by/aurorasoft/testapp/crud/service/AddressService.java
@@ -0,0 +1,79 @@
+package by.aurorasoft.testapp.crud.service;
+
+import by.aurorasoft.replicator.annotation.operation.*;
+import by.aurorasoft.replicator.annotation.service.ReplicatedService;
+import by.aurorasoft.replicator.annotation.service.ReplicatedService.DtoViewConfig;
+import by.aurorasoft.replicator.annotation.service.ReplicatedService.ProducerConfig;
+import by.aurorasoft.replicator.annotation.service.ReplicatedService.TopicConfig;
+import by.aurorasoft.testapp.crud.dto.Address;
+import by.aurorasoft.testapp.crud.entity.AddressEntity;
+import by.aurorasoft.testapp.crud.mapper.AddressMapper;
+import by.aurorasoft.testapp.crud.repository.AddressRepository;
+import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
+import org.apache.kafka.common.serialization.LongSerializer;
+
+import java.util.Collection;
+import java.util.List;
+
+@ReplicatedService(
+        producerConfig = @ProducerConfig(idSerializer = LongSerializer.class),
+        topicConfig = @TopicConfig(name = "address-sync"),
+        dtoViewConfigs = @DtoViewConfig(type = Address.class, includedFields = "country")
+)
+public class AddressService extends AbsServiceCRUD<Long, AddressEntity, Address, AddressRepository> {
+
+    public AddressService(AddressMapper mapper, AddressRepository repository) {
+        super(mapper, repository);
+    }
+
+    @Override
+    @ReplicatedSave
+    public Address save(Address address) {
+        return super.save(address);
+    }
+
+    @Override
+    @ReplicatedSaveAll
+    public List<Address> saveAll(Collection<Address> addresses) {
+        return super.saveAll(addresses);
+    }
+
+    @Override
+    @ReplicatedSave
+    public Address update(Address address) {
+        return super.update(address);
+    }
+
+    @Override
+    @ReplicatedSave
+    public Address updatePartial(Long id, Object partial) {
+        return super.updatePartial(id, partial);
+    }
+
+    @Override
+    @ReplicatedDeleteById
+    public void delete(Long id) {
+        super.delete(id);
+    }
+
+    @ReplicatedDelete
+    public void delete(Address address) {
+        repository.deleteById(address.getId());
+    }
+
+    @ReplicatedDeleteByIds
+    public void deleteByIds(Iterable<Long> ids) {
+        repository.deleteAllById(ids);
+    }
+
+    @ReplicatedDeleteIterable
+    public void delete(Collection<Address> addresses) {
+        List<AddressEntity> entities = mapper.toEntities(addresses);
+        repository.deleteAll(entities);
+    }
+
+    @ReplicatedDeleteAll
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+}
