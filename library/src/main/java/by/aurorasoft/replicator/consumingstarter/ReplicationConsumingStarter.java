11d61193723707fb8ed1aad81c333799e33d7c61
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-09 12:23:42 +0300

    refactor ReplicationConsumingStarter and ReplicationConsumingStarterTest

--- library/src/main/java/by/aurorasoft/replicator/consumingstarter/ReplicationConsumingStarter.java
+++ library/src/main/java/by/aurorasoft/replicator/consumingstarter/ReplicationConsumingStarter.java
@@ -2,6 +2,7 @@ package by.aurorasoft.replicator.consumingstarter;
 
 import by.aurorasoft.replicator.factory.kafkastreams.ReplicationKafkaStreamsFactory;
 import by.aurorasoft.replicator.model.setting.ReplicationConsumeSetting;
+import by.aurorasoft.replicator.validator.ReplicationConsumeSettingUniqueTopicValidator;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.streams.KafkaStreams;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -10,16 +11,16 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-//TODO
 @Component
 @RequiredArgsConstructor
 public final class ReplicationConsumingStarter {
+    private final ReplicationConsumeSettingUniqueTopicValidator uniqueTopicValidator;
     private final ReplicationKafkaStreamsFactory kafkaStreamsFactory;
     private final List<ReplicationConsumeSetting<?, ?>> settings;
 
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
-//        uniqueComponentCheckingManager.check(settings);
+        uniqueTopicValidator.validate(settings);
         settings.stream()
                 .map(kafkaStreamsFactory::create)
                 .forEach(KafkaStreams::start);
