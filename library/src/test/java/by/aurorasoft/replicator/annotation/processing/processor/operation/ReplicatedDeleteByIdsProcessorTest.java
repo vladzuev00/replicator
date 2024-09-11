389f124968f287aa1de953758d7f576781d3f593
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-11 10:34:28 +0300

    refactor ReplicatedDeleteByIdsProcessorTest

--- library/src/test/java/by/aurorasoft/replicator/annotation/processing/processor/operation/ReplicatedDeleteByIdsProcessorTest.java
+++ library/src/test/java/by/aurorasoft/replicator/annotation/processing/processor/operation/ReplicatedDeleteByIdsProcessorTest.java
@@ -1,11 +1,20 @@
 package by.aurorasoft.replicator.annotation.processing.processor.operation;
 
+import by.aurorasoft.replicator.util.AnnotationProcessUtil;
 import org.junit.jupiter.api.Test;
 
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
+import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static by.aurorasoft.replicator.annotation.processing.processor.operation.ReplicatedDeleteByIdsProcessor.PARAMETERS_REQUIREMENT;
+import static by.aurorasoft.replicator.util.AnnotationProcessUtil.isIterable;
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 
 public final class ReplicatedDeleteByIdsProcessorTest {
     private final ReplicatedDeleteByIdsProcessor processor = new ReplicatedDeleteByIdsProcessor();
@@ -20,6 +29,69 @@ public final class ReplicatedDeleteByIdsProcessorTest {
 
     @Test
     public void returnTypeShouldBeValid() {
-        throw new UnsupportedOperationException();
+        TypeMirror givenMirror = mock(TypeMirror.class);
+
+        boolean actual = processor.isValidReturnType(givenMirror);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void parametersShouldBeValid() {
+        try (var mockedAnnotationProcessUtil = mockStatic(AnnotationProcessUtil.class)) {
+            VariableElement givenElement = mock(VariableElement.class);
+            List<? extends VariableElement> givenElements = List.of(givenElement);
+
+            //noinspection ResultOfMethodCallIgnored
+            mockedAnnotationProcessUtil.when(() -> isIterable(same(givenElement))).thenReturn(true);
+
+            boolean actual = processor.isValidParameters(givenElements);
+            assertTrue(actual);
+        }
+    }
+
+    @Test
+    public void parametersShouldNotBeValidBecauseOfNoParameters() {
+        try (var mockedAnnotationProcessUtil = mockStatic(AnnotationProcessUtil.class)) {
+            List<? extends VariableElement> givenElements = emptyList();
+
+            boolean actual = processor.isValidParameters(givenElements);
+            assertFalse(actual);
+
+            mockedAnnotationProcessUtil.verifyNoInteractions();
+        }
+    }
+
+    @Test
+    public void parametersShouldNotBeValidBecauseOfFirstParameterIsNotIterable() {
+        try (var mockedAnnotationProcessUtil = mockStatic(AnnotationProcessUtil.class)) {
+            VariableElement givenElement = mock(VariableElement.class);
+            List<? extends VariableElement> givenElements = List.of(givenElement);
+
+            //noinspection ResultOfMethodCallIgnored
+            mockedAnnotationProcessUtil.when(() -> isIterable(same(givenElement))).thenReturn(false);
+
+            boolean actual = processor.isValidParameters(givenElements);
+            assertFalse(actual);
+        }
+    }
+
+    @Test
+    public void replicatedServiceRequirementShouldNotBeGot() {
+        Optional<String> optionalActual = processor.getReplicatedServiceRequirement();
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @Test
+    public void returnTypeRequirementShouldBeGot() {
+        Optional<String> optionalActual = processor.getReturnTypeRequirement();
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @Test
+    public void parametersRequirementShouldBeGot() {
+        Optional<String> optionalActual = processor.getParametersRequirement();
+        assertTrue(optionalActual.isPresent());
+        String actual = optionalActual.get();
+        assertSame(PARAMETERS_REQUIREMENT, actual);
     }
 }
