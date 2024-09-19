bbe7d5b133a292777d7d323f68731e4581220f0d
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-19 14:55:53 +0300

    refactor

--- library/src/main/java/by/aurorasoft/replicator/annotation/processing/processor/operation/ReplicatedDeleteIterableProcessor.java
+++ library/src/main/java/by/aurorasoft/replicator/annotation/processing/processor/operation/ReplicatedDeleteIterableProcessor.java
@@ -6,14 +6,11 @@ import com.google.auto.service.AutoService;
 import javax.annotation.processing.Processor;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import java.util.List;
 import java.util.Optional;
 
-import static by.aurorasoft.replicator.util.annotationprocessing.AnnotationProcessUtil.getFirstTypeArgument;
-import static by.aurorasoft.replicator.util.annotationprocessing.AnnotationProcessUtil.isContainIdGetter;
-import static by.aurorasoft.replicator.util.annotationprocessing.AnnotationProcessUtil.isIterable;
+import static by.aurorasoft.replicator.util.annotationprocessing.AnnotationProcessUtil.*;
 import static java.util.Optional.empty;
 
 @AutoService(Processor.class)
--- library/src/test/java/by/aurorasoft/replicator/annotation/processing/processor/operation/ReplicatedDeleteProcessorTest.java
+++ library/src/test/java/by/aurorasoft/replicator/annotation/processing/processor/operation/ReplicatedDeleteProcessorTest.java
@@ -1,90 +1,88 @@
-//package by.aurorasoft.replicator.annotation.processing.processor.operation;
-//
-//import org.junit.jupiter.api.Test;
-//import org.mockito.MockedStatic;
-//
-//import javax.lang.model.element.VariableElement;
-//import javax.lang.model.type.TypeMirror;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static by.aurorasoft.replicator.annotation.processing.processor.operation.ReplicatedDeleteProcessor.PARAMETERS_REQUIREMENT;
-//import static by.aurorasoft.replicator.util.annotationprocessing.ElementUtil.isContainIdGetter;
-//import static java.util.Collections.emptyList;
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.ArgumentMatchers.same;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.mockStatic;
-//
-//public final class ReplicatedDeleteProcessorTest {
-//    private final ReplicatedDeleteProcessor processor = new ReplicatedDeleteProcessor();
-//
-//    @Test
-//    public void replicatedServiceShouldBeValid() {
-//        TypeMirror givenMirror = mock(TypeMirror.class);
-//
-//        assertTrue(processor.isValidReplicatedService(givenMirror));
-//    }
-//
-//    @Test
-//    public void returnTypeShouldBeValid() {
-//        TypeMirror givenMirror = mock(TypeMirror.class);
-//
-//        assertTrue(processor.isValidReturnType(givenMirror));
-//    }
-//
-//    @Test
-//    public void parametersShouldBeValid() {
-//        try (MockedStatic<ElementUtil> mockedElementUtil = mockStatic(ElementUtil.class)) {
-//            VariableElement givenFirstParameter = mock(VariableElement.class);
-//            List<? extends VariableElement> givenElements = List.of(givenFirstParameter);
-//
-//            mockedElementUtil.when(() -> isContainIdGetter(same(givenFirstParameter))).thenReturn(true);
-//
-//            assertTrue(processor.isValidParameters(givenElements));
-//        }
-//    }
-//
-//    @Test
-//    public void parametersShouldNotBeValidBecauseOfNoParameters() {
-//        try (MockedStatic<ElementUtil> mockedElementUtil = mockStatic(ElementUtil.class)) {
-//            List<? extends VariableElement> givenElements = emptyList();
-//
-//            assertFalse(processor.isValidParameters(givenElements));
-//
-//            mockedElementUtil.verifyNoInteractions();
-//        }
-//    }
-//
-//    @Test
-//    public void parametersShouldNotBeValidBecauseOfFirstParameterNotContainId() {
-//        try (MockedStatic<ElementUtil> mockedElementUtil = mockStatic(ElementUtil.class)) {
-//            VariableElement givenFirstParameter = mock(VariableElement.class);
-//            List<? extends VariableElement> givenElements = List.of(givenFirstParameter);
-//
-//            mockedElementUtil.when(() -> isContainIdGetter(same(givenFirstParameter))).thenReturn(false);
-//
-//            assertFalse(processor.isValidParameters(givenElements));
-//        }
-//    }
-//
-//    @Test
-//    public void replicatedServiceRequirementShouldNotBeGot() {
-//        Optional<String> optionalActual = processor.getReplicatedServiceRequirement();
-//        assertTrue(optionalActual.isEmpty());
-//    }
-//
-//    @Test
-//    public void returnTypeRequirementShouldNotBeGot() {
-//        Optional<String> optionalActual = processor.getReturnTypeRequirement();
-//        assertTrue(optionalActual.isEmpty());
-//    }
-//
-//    @Test
-//    public void parametersRequirementShouldBeGot() {
-//        Optional<String> optionalActual = processor.getParametersRequirement();
-//        assertTrue(optionalActual.isPresent());
-//        String actual = optionalActual.get();
-//        assertSame(PARAMETERS_REQUIREMENT, actual);
-//    }
-//}
+package by.aurorasoft.replicator.annotation.processing.processor.operation;
+
+import org.junit.jupiter.api.Test;
+
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
+import java.util.List;
+import java.util.Optional;
+
+import static by.aurorasoft.replicator.annotation.processing.processor.operation.ReplicatedDeleteProcessor.PARAMETERS_REQUIREMENT;
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+
+public final class ReplicatedDeleteProcessorTest {
+    private final ReplicatedDeleteProcessor processor = new ReplicatedDeleteProcessor();
+
+    @Test
+    public void replicatedServiceShouldBeValid() {
+        TypeMirror givenMirror = mock(TypeMirror.class);
+
+        assertTrue(processor.isValidReplicatedService(givenMirror));
+    }
+
+    @Test
+    public void returnTypeShouldBeValid() {
+        TypeMirror givenMirror = mock(TypeMirror.class);
+
+        assertTrue(processor.isValidReturnType(givenMirror));
+    }
+
+    @Test
+    public void parametersShouldBeValid() {
+        try (MockedStatic<ElementUtil> mockedElementUtil = mockStatic(ElementUtil.class)) {
+            VariableElement givenFirstParameter = mock(VariableElement.class);
+            List<? extends VariableElement> givenElements = List.of(givenFirstParameter);
+
+            mockedElementUtil.when(() -> isContainIdGetter(same(givenFirstParameter))).thenReturn(true);
+
+            assertTrue(processor.isValidParameters(givenElements));
+        }
+    }
+
+    @Test
+    public void parametersShouldNotBeValidBecauseOfNoParameters() {
+        try (MockedStatic<ElementUtil> mockedElementUtil = mockStatic(ElementUtil.class)) {
+            List<? extends VariableElement> givenElements = emptyList();
+
+            assertFalse(processor.isValidParameters(givenElements));
+
+            mockedElementUtil.verifyNoInteractions();
+        }
+    }
+
+    @Test
+    public void parametersShouldNotBeValidBecauseOfFirstParameterNotContainId() {
+        try (MockedStatic<ElementUtil> mockedElementUtil = mockStatic(ElementUtil.class)) {
+            VariableElement givenFirstParameter = mock(VariableElement.class);
+            List<? extends VariableElement> givenElements = List.of(givenFirstParameter);
+
+            mockedElementUtil.when(() -> isContainIdGetter(same(givenFirstParameter))).thenReturn(false);
+
+            assertFalse(processor.isValidParameters(givenElements));
+        }
+    }
+
+    @Test
+    public void replicatedServiceRequirementShouldNotBeGot() {
+        Optional<String> optionalActual = processor.getReplicatedServiceRequirement();
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @Test
+    public void returnTypeRequirementShouldNotBeGot() {
+        Optional<String> optionalActual = processor.getReturnTypeRequirement();
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @Test
+    public void parametersRequirementShouldBeGot() {
+        Optional<String> optionalActual = processor.getParametersRequirement();
+        assertTrue(optionalActual.isPresent());
+        String actual = optionalActual.get();
+        assertSame(PARAMETERS_REQUIREMENT, actual);
+    }
+}
