2b4c7df789a83fd7235105d836d31af96d766d8d
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-20 11:04:48 +0300

    add AnnotationProcessUtil::isPublic

--- library/src/test/java/by/aurorasoft/replicator/util/annotationprocessing/AnnotationProcessUtilTest.java
+++ library/src/test/java/by/aurorasoft/replicator/util/annotationprocessing/AnnotationProcessUtilTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static by.aurorasoft.replicator.util.annotationprocessing.AnnotationProcessUtil.getAnnotatedElements;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static by.aurorasoft.replicator.util.annotationprocessing.AnnotationProcessUtil.isPublic;
+import static javax.lang.model.element.Modifier.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -36,5 +39,25 @@ public final class AnnotationProcessUtilTest {
         List<ExecutableElement> actualAsList = actual.toList();
         List<Element> expectedAsList = List.of(firstGivenElement, secondGivenElement, thirdGivenElement);
         assertEquals(expectedAsList, actualAsList);
+    }
+
+    @Test
+    public void elementShouldBePublic() {
+        Element givenElement = mock(Element.class);
+
+        Set<Modifier> givenModifiers = Set.of(NATIVE, SYNCHRONIZED, PUBLIC, VOLATILE);
+        when(givenElement.getModifiers()).thenReturn(givenModifiers);
+
+        assertTrue(isPublic(givenElement));
+    }
+
+    @Test
+    public void elementShouldNotBePublic() {
+        Element givenElement = mock(Element.class);
+
+        Set<Modifier> givenModifiers = Set.of(NATIVE, SYNCHRONIZED, PRIVATE, VOLATILE);
+        when(givenElement.getModifiers()).thenReturn(givenModifiers);
+
+        assertFalse(isPublic(givenElement));
     }
 }
