c914c0e218602199abd2ff5ac46287e0d75b0382
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-20 10:43:19 +0300

    refactor ReplicatedServiceUniqueTopicValidatorTest

--- library/src/test/java/by/aurorasoft/replicator/validator/ReplicatedServiceUniqueTopicValidatorTest.java
+++ library/src/test/java/by/aurorasoft/replicator/validator/ReplicatedServiceUniqueTopicValidatorTest.java
@@ -1,18 +1,23 @@
 package by.aurorasoft.replicator.validator;
 
+import by.aurorasoft.replicator.base.AbstractSpringBootTest;
 import by.aurorasoft.replicator.testcrud.TestService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public final class ReplicatedServiceUniqueTopicValidatorTest {
-    private final ReplicatedServiceUniqueTopicValidator validator = new ReplicatedServiceUniqueTopicValidator();
+public final class ReplicatedServiceUniqueTopicValidatorTest extends AbstractSpringBootTest {
+
+    @Autowired
+    private ReplicatedServiceUniqueTopicValidator validator;
+
+    @Autowired
+    private TestService service;
 
     @Test
     public void propertyShouldBeGot() {
-        TestService givenService = new TestService(null);
-
-        String actual = validator.getProperty(givenService);
+        String actual = validator.getProperty(service);
         String expected = "sync-dto";
         assertEquals(expected, actual);
     }
