f504fe2ad09f973d60a3d81d6d8bac0793b23aab
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-23 10:41:05 +0300

    refactor TestService

--- library/src/test/java/by/aurorasoft/replicator/testcrud/TestService.java
+++ library/src/test/java/by/aurorasoft/replicator/testcrud/TestService.java
@@ -1,5 +1,6 @@
 package by.aurorasoft.replicator.testcrud;
 
+import by.aurorasoft.replicator.annotation.operation.*;
 import by.aurorasoft.replicator.annotation.service.ReplicatedService;
 import by.aurorasoft.replicator.annotation.service.ReplicatedService.DtoViewConfig;
 import by.aurorasoft.replicator.annotation.service.ReplicatedService.ProducerConfig;
@@ -30,39 +31,39 @@ public class TestService {
     @SuppressWarnings("unused")
     private final TestRepository repository;
 
-//    @ReplicatedSave
+    @ReplicatedSave
     public TestDto save(TestDto dto) {
         return dto;
     }
 
-//    @ReplicatedSaveAll
+    @ReplicatedSaveAll
     public List<TestDto> saveAll(Iterable<TestDto> dtos) {
         List<TestDto> result = new ArrayList<>();
         dtos.forEach(result::add);
         return result;
     }
 
-//    @ReplicatedDeleteById
+    @ReplicatedDeleteById
     public void deleteById(@SuppressWarnings("unused") Long id) {
 
     }
 
-//    @ReplicatedDelete
+    @ReplicatedDelete
     public void delete(@SuppressWarnings("unused") TestDto dto) {
 
     }
 
-//    @ReplicatedDeleteByIds
+    @ReplicatedDeleteByIds
     public void deleteByIds(@SuppressWarnings("unused") Iterable<Long> ids) {
 
     }
 
-//    @ReplicatedDeleteIterable
+    @ReplicatedDeleteIterable
     public void delete(@SuppressWarnings("unused") Iterable<TestDto> dtos) {
 
     }
 
-//    @ReplicatedDeleteAll
+    @ReplicatedDeleteAll
     public void deleteAll() {
 
     }
