aa246e70fe43c29d3cc0a570ad0be8dc10893acc
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-24 14:56:23 +0300

    add DeclaredTypeUtil

---
+++ library/src/main/java/by/aurorasoft/replicator/util/annotationprocessing/DeclaredTypeUtil.java
@@ -0,0 +1,19 @@
+package by.aurorasoft.replicator.util.annotationprocessing;
+
+import lombok.experimental.UtilityClass;
+
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
+import java.util.List;
+
+@UtilityClass
+public final class DeclaredTypeUtil {
+
+    public static TypeMirror getFirstTypeArgument(DeclaredType type) {
+        List<? extends TypeMirror> arguments = type.getTypeArguments();
+        if (arguments.isEmpty()) {
+            throw new IllegalArgumentException("There are no type arguments in '%s'".formatted(type));
+        }
+        return arguments.get(0);
+    }
+}
---
+++ library/src/test/java/by/aurorasoft/replicator/annotation/processing/processor/operation/DeclaredTypeUtilTest.java
@@ -0,0 +1,16 @@
+package by.aurorasoft.replicator.annotation.processing.processor.operation;
+
+import org.junit.jupiter.api.Test;
+
+public final class DeclaredTypeUtilTest {
+
+    @Test
+    public void firstTypeArgumentShouldBeGot() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void firstTypeArgumentShouldNotBeGot() {
+        throw new RuntimeException();
+    }
+}
