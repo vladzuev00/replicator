40c114dfd1e91293e1ad1fe1309427d0c879e541
Vlad Zuev <vladzuev.00@mail.ru>
2024-09-27 17:21:02 +0300

    refactor AbstractSpringBootTest

--- test-application/src/test/java/by/aurorasoft/testapp/base/AbstractSpringBootTest.java
+++ test-application/src/test/java/by/aurorasoft/testapp/base/AbstractSpringBootTest.java
@@ -1,6 +1,5 @@
 package by.aurorasoft.testapp.base;
 
-import by.aurorasoft.testapp.ApplicationRunner;
 import by.aurorasoft.testapp.base.containerinitializer.DataBaseContainerInitializer;
 import by.aurorasoft.testapp.base.containerinitializer.KafkaContainerInitializer;
 import jakarta.persistence.EntityManager;
@@ -9,16 +8,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import static java.time.ZoneOffset.UTC;
 import static java.util.TimeZone.getTimeZone;
 import static java.util.TimeZone.setDefault;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
-import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
 
 @SpringBootTest
-//@Transactional(propagation = NOT_SUPPORTED)
 @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 @ContextConfiguration(initializers = {DataBaseContainerInitializer.class, KafkaContainerInitializer.class})
 public abstract class AbstractSpringBootTest {
